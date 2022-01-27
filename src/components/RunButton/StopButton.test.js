542e78a5ecbb59e866a60413dc7e5105b8503f43
Lois Wells <lois.wells@raspberrypi.org>
2022-01-27 14:21:49 +0000

    Tidying up

--- src/components/RunButton/StopButton.test.js
+++ src/components/RunButton/StopButton.test.js
@@ -11,10 +11,15 @@ import { codeRunHandled, triggerCodeRun } from '../Editor/EditorSlice'
 test("Clicking stop button without input box sets codeRunStopped to true", () => {
     store.dispatch(codeRunHandled())
     store.dispatch(triggerCodeRun())
+
+    const { getByText } = render(
+    <Provider store={store}>
+        <StopButton buttonText="Stop Code" />
+    </Provider>);
     
-    const { getByText } = render(<Provider store={store}><StopButton buttonText="Stop Code" /></Provider>);
     const stopButton = getByText(/Stop Code/);
     fireEvent.click(stopButton);
+
     expect(store.getState().editor.codeRunStopped).toEqual(true);
 })
 
@@ -24,24 +29,28 @@ test("Clicking stop button with input box handles code run", () => {
 
     const { getByText } = render(
     <Provider store={store}>
-        {/* <RunButton buttonText="Run Code" /> */}
         <StopButton buttonText="Stop Code" />
         <span id="input" contentEditable="true">hello world</span>
     </Provider>
     );
-   
     const stopButton = getByText(/Stop Code/);
     fireEvent.click(stopButton);
+
     expect(store.getState().editor.codeRunStopped).toEqual(false);
     expect(store.getState().editor.codeRunTriggered).toEqual(false);
 })
 
 test("Clicking stop button removes editable input box but keeps input text", () => {
-    const { getByText } = render(<Provider store={store}><StopButton buttonText="Stop Code" /><span id="input" contentEditable="true">hello world</span></Provider>);
+    const { getByText } = render(
+    <Provider store={store}>
+        <StopButton buttonText="Stop Code" />
+        <span id="input" contentEditable="true">hello world</span>
+    </Provider>);
+
     const stopButton = getByText(/Stop Code/);
     const inputSpan = document.getElementById("input");
-    // expect(inputSpan).toEqual(<span id="input" contentEditable="true">hello world</span>);
     fireEvent.click(stopButton);
+
     expect(inputSpan.textContent).toEqual("hello world");
     expect(inputSpan).not.toHaveAttribute("contentEditable", "true");
     expect(inputSpan).not.toHaveAttribute("id", "input");
