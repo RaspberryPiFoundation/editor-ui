93380875695a9aa7c345eb9f6c130394b5b0dadd
Lois Wells <lois.wells@raspberrypi.org>
2022-02-22 16:01:02 +0000

    Fixing indentation

--- src/components/Editor/Project/Project.test.js
+++ src/components/Editor/Project/Project.test.js
@@ -9,71 +9,71 @@ import axios from "axios";
 jest.mock('axios');
 
 jest.mock('react-router-dom', () => ({
-    ...jest.requireActual('react-router-dom'),
-    useHistory: () => ({
-      push: jest.fn()
-    })
-  }));
+  ...jest.requireActual('react-router-dom'),
+  useHistory: () => ({
+    push: jest.fn()
+  })
+}));
 
 describe("Testing the remix button when logged in", () => {
-    let store;
-    let remixButton;
+  let store;
+  let remixButton;
     
-    beforeEach(() => {
-        const middlewares = []
-        const mockStore = configureStore(middlewares)
-        const initialState = {
-            editor: {
-                project: {
-                    identifier: "hello-world-project",
-                    components: []
-                },
-            },
-            auth: {
-                user: []
-            }
-        }
-        store = mockStore(initialState);
-        const {getByText} = render(<Provider store={store}><Project/></Provider>);
-        remixButton = getByText(/Remix/)
-    })
+  beforeEach(() => {
+    const middlewares = []
+    const mockStore = configureStore(middlewares)
+    const initialState = {
+      editor: {
+        project: {
+          identifier: "hello-world-project",
+          components: []
+        },
+      },
+      auth: {
+        user: []
+      }
+    }
+    store = mockStore(initialState);
+    const {getByText} = render(<Provider store={store}><Project/></Provider>);
+    remixButton = getByText(/Remix/)
+  })
 
-    test("Remix button renders when the user is logged in", () => {
-        expect(remixButton.textContent).toBe("Remix Project");
-    })
+  test("Remix button renders when the user is logged in", () => {
+    expect(remixButton.textContent).toBe("Remix Project");
+  })
 
-    test("Clicking remix button posts to correct remix url", () => {
-        axios.post.mockImplementationOnce(() => Promise.resolve({'data': { 'project': {'identifier': "remixed-hello-project", 'project_type': 'python'}}}))
+  test("Clicking remix button posts to correct remix url", () => {
+    axios.post.mockImplementationOnce(() => Promise.resolve({'data': { 'project': {'identifier': 'remixed-hello-project', 'project_type': 'python'}}}))
 
-        fireEvent.click(remixButton)
-        const api_host = process.env.REACT_APP_API_ENDPOINT;
-        expect(axios.post).toHaveBeenCalledWith(`${api_host}/api/projects/phrases/${store.getState()['editor']['project']['identifier']}/remix`)
+    fireEvent.click(remixButton)
+    const api_host = process.env.REACT_APP_API_ENDPOINT;
+    expect(axios.post).toHaveBeenCalledWith(`${api_host}/api/projects/phrases/${store.getState()['editor']['project']['identifier']}/remix`)
 
-    })
+  })
 })
 
 describe("Testing the remix button when not logged in", () => {
-    let queryByText;
+  let queryByText;
     
-    beforeEach(() => {
-        const middlewares = []
-        const mockStore = configureStore(middlewares)
-        const initialState = {
-            editor: {
-                project: {
-                    identifier: "hello-world-project",
-                    components: []
-                },
-            },
-            auth: {
-                user: null
-            }
+  beforeEach(() => {
+    const middlewares = []
+    const mockStore = configureStore(middlewares)
+    const initialState = {
+        editor: {
+          project: {
+            identifier: "hello-world-project",
+            components: []
+        },
+      },
+        auth: {
+          user: null
         }
-        const store = mockStore(initialState);
-         ({queryByText} = render(<Provider store={store}><Project/></Provider>));
-    })
+      }
+    const store = mockStore(initialState);
+    ({queryByText} = render(<Provider store={store}><Project/></Provider>));
+  })
 
-    test("No remix button when not logged in", () => {
-        expect(queryByText('Remix')).toBeNull();
-    })
+  test("No remix button when not logged in", () => {
+    expect(queryByText('Remix')).toBeNull();
+  })
 })
