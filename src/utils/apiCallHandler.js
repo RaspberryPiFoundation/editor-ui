875f0e5e11b659b55f5920553a4bf1417044dbca
Lois Wells <lois.wells@raspberrypi.org>
2022-02-25 11:33:56 +0000

    Abstracting out the headers

--- src/utils/apiCallHandler.js
+++ src/utils/apiCallHandler.js
@@ -2,43 +2,39 @@ import axios from 'axios';
 
 const host = process.env.REACT_APP_API_ENDPOINT;
 
-const get = async (url) => {
-  return await axios.get(url, {headers: {
-      'Accept': 'application/json'
-    }
-  });
+const get = async (url, headers) => {
+  return await axios.get(url, headers);
 }
 
-const post = async (url, body) => {
-  if (!body) {
-    body = {}
-  }
-  return await axios.post(url, body, { 
-    headers: {
-      'Accept': 'application/json'
-    }
-  });
+const post = async (url, body, headers) => {
+  return await axios.post(url, body, headers);
 }
 
-const put = async (url, body) => {
-  return await axios.put(url, body)
+const put = async (url, body, headers) => {
+  return await axios.put(url, body, headers)
+}
+
+const headers = () => {
+    var headersHash = {'Accept': 'application/json'}
+    return {headers: headersHash}
 }
 
 export const updateProject = async (project) => {
   return await put(
     `${host}/api/projects/phrases/${project.identifier}`,
-    { project: project }
+    { project: project }, 
+    headers()
   );
 }
 
 export const newProject = async () => {
-  return await post(`${host}/api/default_project/`);
+  return await post(`${host}/api/default_project/`, {}, headers());
 }
 
 export const remixProject = async (projectIdentifier) => {
-  return await post(`${host}/api/projects/phrases/${projectIdentifier}/remix`);
+  return await post(`${host}/api/projects/phrases/${projectIdentifier}/remix`, {}, headers());
 }
 
 export const readProject = async (projectIdentifier) => {
-  return await get(`${host}/api/projects/phrases/${projectIdentifier}`);
+  return await get(`${host}/api/projects/phrases/${projectIdentifier}`, headers());
 }
