0cce987bc12df858790fdf2efbf4bc9617004a43
Lois Wells <lois.wells@raspberrypi.org>
2022-02-25 15:18:55 +0000

    Tidying up

--- src/utils/apiCallHandler.test.js
+++ src/utils/apiCallHandler.test.js
@@ -8,42 +8,43 @@ const defaultHeaders = {'headers': {'Accept': 'application/json'}}
 
 describe("Mocked API calls", () => {
 
-    test("Creating project", async () => {
-        const blankProject =  {'data': { 'project': {'identifier': 'new-hello-project', 'project_type': 'python'}}}
-        axios.post.mockImplementationOnce(() => Promise.resolve(blankProject))
-        
-        await newProject()
-        expect(axios.post).toHaveBeenCalledWith(`${host}/api/default_project/`, {}, defaultHeaders)
-    })
+  test("Creating project", async () => {
+    const blankProject =  {'data': { 'project': {'identifier': 'new-hello-project', 'project_type': 'python'}}}
+    axios.post.mockImplementationOnce(() => Promise.resolve(blankProject))
     
-    test("Remixing project", async () => {
-        const originalProject = {'identifier': 'original-hello-project', 'project_type': 'python'}
-        axios.post.mockImplementationOnce((url, body, headers) => {
-            const remixedProject = {'identifier': 'remixed-hello-project', 'project_type': 'python'}
-            Promise.resolve({data: {project: remixedProject}})
-        })
-
-        await remixProject(originalProject['identifier'])
-        expect(axios.post).toHaveBeenCalledWith((`${host}/api/projects/phrases/${originalProject['identifier']}/remix`), {}, defaultHeaders)
-
+    await newProject()
+    expect(axios.post).toHaveBeenCalledWith(`${host}/api/default_project/`, {}, defaultHeaders)
+  })
+    
+  test("Remixing project", async () => {
+    const originalProject = {'identifier': 'original-hello-project', 'project_type': 'python'}
+    axios.post.mockImplementationOnce((url, body, headers) => {
+      const remixedProject = {'identifier': 'remixed-hello-project', 'project_type': 'python'}
+      Promise.resolve({data: {project: remixedProject}})
     })
 
-    test("Updating project", async () => {
-        const project = {'identifier': 'my-wonderful-project', 'project_type': 'python', 'components': []}
-        axios.put.mockImplementationOnce(() => Promise.resolve(200))
+    await remixProject(originalProject['identifier'])
+    expect(axios.post).toHaveBeenCalledWith((`${host}/api/projects/phrases/${originalProject['identifier']}/remix`), {}, defaultHeaders)
+  })
 
-        await updateProject(project)
-        expect(axios.put).toHaveBeenCalledWith(`${host}/api/projects/phrases/${project['identifier']}`,
-        { project: project }, 
-        defaultHeaders)
-    })
+  test("Updating project", async () => {
+    const project = {'identifier': 'my-wonderful-project', 'project_type': 'python', 'components': []}
+    axios.put.mockImplementationOnce(() => Promise.resolve(200))
 
-    test("Read project", async () => {
-        const projectIdentifier = "hello-world-project"
-        const projectData =  {'data': { 'project': {'identifier': projectIdentifier, 'project_type': 'python'}}}
-        axios.get.mockImplementationOnce(() => Promise.resolve(projectData))
-        
-        await readProject(projectIdentifier)
-        expect(axios.get).toHaveBeenCalledWith(`${host}/api/projects/phrases/${projectIdentifier}`, defaultHeaders)
-    })
+    await updateProject(project)
+    expect(axios.put).toHaveBeenCalledWith(
+      `${host}/api/projects/phrases/${project['identifier']}`,
+      { project: project }, 
+      defaultHeaders
+      )
+  })
+
+  test("Read project", async () => {
+    const projectIdentifier = "hello-world-project"
+    const projectData =  {'data': { 'project': {'identifier': projectIdentifier, 'project_type': 'python'}}}
+    axios.get.mockImplementationOnce(() => Promise.resolve(projectData))
+
+    await readProject(projectIdentifier)
+     expect(axios.get).toHaveBeenCalledWith(`${host}/api/projects/phrases/${projectIdentifier}`, defaultHeaders)
+  })
 })
