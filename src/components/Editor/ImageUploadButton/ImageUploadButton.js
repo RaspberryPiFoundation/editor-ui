1ee333a2a188e3503c73e7c7c8a665da0f2f1d37
Lois Wells <lois.wells@raspberrypi.org>
2022-03-11 15:30:40 +0000

    Bringing modals to the front

--- src/components/Editor/ImageUploadButton/ImageUploadButton.js
+++ src/components/Editor/ImageUploadButton/ImageUploadButton.js
@@ -95,6 +95,9 @@ const ImageUploadButton = () => {
         marginRight: '-50%',
         transform: 'translate(-50%, -50%)',
       },
+      overlay: {
+        zIndex: 1000
+      }
     };
   
     return (
