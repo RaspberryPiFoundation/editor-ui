f9a9bd99464f057e38926beb643ebd01f026e0e8
Lois Wells <lois.wells@raspberrypi.org>
2022-05-06 11:58:14 +0100

    Initial update to react and react-dom version 18

--- src/App.test.js
+++ src/App.test.js
@@ -2,11 +2,12 @@ import App from './App';
 import store from './app/store'
 import { Provider } from 'react-redux'
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {createRoot} from 'react-dom/client';
 
 test('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(
+  const root = createRoot(div)
+  root.render(
     <Provider store={store}>
       <App />
     </Provider>,
