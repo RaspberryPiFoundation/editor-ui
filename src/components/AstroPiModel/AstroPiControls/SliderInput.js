7850b69906dc1c61238ac63ed15f94aea7e6d299
Lois Wells <lois.wells@raspberrypi.org>
2022-05-27 14:39:28 +0100

    Set icon colours by theme

--- src/components/AstroPiModel/AstroPiControls/SliderInput.js
+++ src/components/AstroPiModel/AstroPiControls/SliderInput.js
@@ -3,10 +3,13 @@ import { useEffect, useState } from 'react';
 import { Barometer, Humidity, Thermometer } from "@intern0t/react-weather-icons";
 import '../AstroPiModel.scss';
 import Sk from 'skulpt';
+import { useSelector } from 'react-redux';
 
 const SliderInput = (props) => {
-  const { name, unit, iconClass, min, max, defaultValue} = props;
+  const { name, unit, min, max, defaultValue} = props;
   const [value, setValue] = useState(defaultValue);
+  const isDarkMode = useSelector((state) => state.editor.darkModeEnabled)
+  const iconColour = isDarkMode ? "white" : "black"
 
   useEffect(() => {
     if (Sk.sense_hat) {
@@ -17,7 +20,7 @@ const SliderInput = (props) => {
   return (
     <div className="rangeslider-container">
       <div className="readings-container">
-        {name==="temperature" ? <Thermometer /> : name==="pressure" ? <Barometer />: name==="humidity"? <Humidity /> : null}
+        {name==="temperature" ? <Thermometer color={iconColour}/> : name==="pressure" ? <Barometer color={iconColour}/>: name==="humidity"? <Humidity color={iconColour} /> : null}
         <span className={`sensor-value sense-hat-${name}`}>{value}{unit}</span>
       </div>
       <input id={`sense_hat_${name}`} className="rangeslider" type="range" min={min} max={max} step="1" defaultValue={value} onChange={e => setValue(parseFloat(e.target.value))}/>
--- src/components/AstroPiModel/OrientationPanel/OrientationResetButton.js
+++ src/components/AstroPiModel/OrientationPanel/OrientationResetButton.js
@@ -2,14 +2,16 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faRefresh } from '@fortawesome/free-solid-svg-icons'
 import '../AstroPiModel.scss';
+import { useSelector } from 'react-redux';
 
 const OrientationResetButton = (props) => {
 
   const {resetOrientation} = props;
+  const isDarkMode = useSelector((state) => state.editor.darkModeEnabled)
 
   return (
     <div id="close-sense-hat-orientation-controls">
-      <button id="orientation-reset-btn" onClick={e => resetOrientation(e)}><FontAwesomeIcon icon={faRefresh} size="2x"/></button>
+      <button id="orientation-reset-btn" onClick={e => resetOrientation(e)}><FontAwesomeIcon icon={faRefresh} size="2x" color={isDarkMode?"white":"black"}/></button>
     </div>
   )
 }
