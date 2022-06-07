26b11b730a316cc9d925686001089980b598383e
Lois Wells <lois.wells@raspberrypi.org>
2022-06-07 15:43:46 +0100

    Some astro pi model dark mode fixes

--- src/setupTests.js
+++ src/setupTests.js
@@ -4,3 +4,13 @@
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
 globalThis.IS_REACT_ACT_ENVIRONMENT = true;
+window.matchMedia = (query) => ({
+  matches: false,
+  media: query,
+  onchange: null,
+  addListener: jest.fn(), // Deprecated
+  removeListener: jest.fn(), // Deprecated
+  addEventListener: jest.fn(),
+  removeEventListener: jest.fn(),
+  dispatchEvent: jest.fn(),
+})
