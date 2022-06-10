647f333e6da4fbc91188fd00d10b7aaecbad957e
Lois Wells <lois.wells@raspberrypi.org>
2022-06-10 15:47:56 +0100

    Tiny bit of styling

--- src/components/AstroPiModel/AstroPiControls/Stopwatch.js
+++ src/components/AstroPiModel/AstroPiControls/Stopwatch.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 import { useSelector } from 'react-redux';
 import { useStopwatch } from 'react-timer-hook';
+import './Stopwatch.scss'
 
 const Stopwatch = () => {
   const codeRunTriggered = useSelector((state) => state.editor.codeRunTriggered)
@@ -17,8 +18,11 @@ const Stopwatch = () => {
 
 
   return (
-    <div>
-      <span>{String(minutes).padStart(2, '0')}</span>:<span>{String(seconds).padStart(2, '0')}</span>
+    <div className='astro-pi-stopwatch-container'>
+      <label htmlFor='astro_pi_stopwatch'>Timer: </label>
+      <span className='astro-pi-stopwatch' id='astro_pi_stopwatch'>
+        <span>{String(minutes).padStart(2, '0')}</span>:<span>{String(seconds).padStart(2, '0')}</span>
+      </span>
     </div>
   )
 }
