e38f7aa5c897f7b88bd114128234f5407d3f10fc
Lois Wells <lois.wells@raspberrypi.org>
2022-06-14 13:01:18 +0100

    Getting cypress container working

--- e2e/cypress/integration/spec.js
+++ e2e/cypress/integration/spec.js
@@ -3,9 +3,11 @@
 //   cy.get(".editor-controls button").should("contain", "Run Code")
 // })
 
-it("renders the page", () => {
+it("renders the web component", () => {
   cy.visit("/")
   // cy.get(".editor-controls button").should("contain", "Run Code")
-  cy.get("editor-wc").shadow().find("button").its('length').should('be.gt', 0);
+  // cy.get("editor-wc").shadow().find("button").its('length').should('be.gt', 0);
+  cy.get("editor-wc").shadow().find("button").should("contain", "Run Code")
+
 })
 
