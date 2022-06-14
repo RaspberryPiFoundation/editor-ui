5edde20f2ab8f676e2e94c75894d9eacc8cc1cc2
Lois Wells <lois.wells@raspberrypi.org>
2022-06-14 15:17:08 +0100

    Trying to ontegrate cypress with circleCI

---
+++ e2e/cypress/integration/spec-wc.js
@@ -0,0 +1,12 @@
+const baseUrl = "http://react-ui-test-wc:9000"
+
+it("renders the web component", () => {
+  cy.visit(baseUrl)
+  cy.get("editor-wc").shadow().find("button").should("contain", "Run Code")
+})
+
+// it("runs the python code", () => {
+//   cy.visit("/")
+//   cy.get("editor-wc").shadow().find("div[class=cm-content]").type("\nprint('Hello world')")
+//   cy.get("editor-wc").shadow().find("button").click()
+// })
--- e2e/cypress/integration/spec.js
+++ e2e/cypress/integration/spec.js
@@ -1,11 +1,8 @@
+const baseUrl = "http://react-ui-test:3000"
+
 it("renders the web component", () => {
-  cy.visit("/")
-  cy.get("editor-wc").shadow().find("button").should("contain", "Run Code")
+  cy.visit(baseUrl)
+  cy.get(".editor-controls button").should("contain", "Run Code")
 })
 
-// it("runs the python code", () => {
-//   cy.visit("/")
-//   cy.get("editor-wc").shadow().find("div[class=cm-content]").type("\nprint('Hello world')")
-//   cy.get("editor-wc").shadow().find("button").click()
-// })
 
