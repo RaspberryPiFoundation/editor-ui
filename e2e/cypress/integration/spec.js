f70a5693fddd3a6b0da7cc1f192c9b7443e4b529
Lois Wells <lois.wells@raspberrypi.org>
2022-06-14 14:28:03 +0100

    Trying to fix gitignore

--- e2e/cypress/integration/spec.js
+++ e2e/cypress/integration/spec.js
@@ -1,13 +1,11 @@
-// it("renders the page", () => {
-//   cy.visit("/")
-//   cy.get(".editor-controls button").should("contain", "Run Code")
-// })
-
 it("renders the web component", () => {
   cy.visit("/")
-  // cy.get(".editor-controls button").should("contain", "Run Code")
-  // cy.get("editor-wc").shadow().find("button").its('length').should('be.gt', 0);
   cy.get("editor-wc").shadow().find("button").should("contain", "Run Code")
-
 })
+
+// it("runs the python code", () => {
+//   cy.visit("/")
+//   cy.get("editor-wc").shadow().find("div[class=cm-content]").type("\nprint('Hello world')")
+//   cy.get("editor-wc").shadow().find("button").click()
+// })
 
