c209c3665ee6d17c31c47e314209983c90ff2921
Lois Wells <lois.wells@raspberrypi.org>
2022-06-21 09:27:12 +0100

    Trying to fix screenshot problem

--- cypress.config.mjs
+++ cypress.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
   e2e: {
     supportFile: false,
-    defaultCommandTimeout: 10000
+    defaultCommandTimeout: 10000,
+    screenshotOnRunFailure: false
   }
 })
