ff28ac808264947473bc2a5af29d686b0a5db278
Lois Wells <lois.wells@raspberrypi.org>
2022-07-18 16:10:51 +0100

    Fixing broken tests

--- src/components/Login/Login.test.js
+++ src/components/Login/Login.test.js
@@ -1,4 +1,6 @@
 import React from "react";
+import store from '../../app/store'
+import { MemoryRouter } from "react-router-dom";
 import { render } from "@testing-library/react"
 import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
@@ -7,14 +9,14 @@ import Login from "./Login";
 
 test("Login button shown when not logged in", () => {
   const user = null
-  const { getByText} = render(<Login user={user} />);
+  const { getByText} = render(<MemoryRouter initialEntries={['/']}><Provider store={store}><Login user={user} /></Provider></MemoryRouter>);
 
   expect(getByText(/Log/).textContent).toBe("Login")
 })
 
 test("Logout button shown when user logged in", () => {
   const user = { profile: { email: 'test@example.com' }}
-  const {getByText} = render(<Login user={user} />)
+  const {getByText} = render(<MemoryRouter initialEntries={['/']}><Provider store={store}><Login user={user} /></Provider></MemoryRouter>)
 
   expect(getByText(/Log/).textContent).toBe("Logout")
 })
