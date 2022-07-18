f643dcda7ff933eb310e85a6c1dc42c4b406eb8b
Lois Wells <lois.wells@raspberrypi.org>
2022-07-18 16:22:57 +0100

    tidying

--- src/components/Login/Login.test.js
+++ src/components/Login/Login.test.js
@@ -3,7 +3,6 @@ import store from '../../app/store'
 import { MemoryRouter } from "react-router-dom";
 import { render } from "@testing-library/react"
 import { Provider } from 'react-redux';
-import configureStore from 'redux-mock-store';
 
 import Login from "./Login";
 
