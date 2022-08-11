b535f46678b3e1c669b9fa137bb5b923caba9ea7
Lois Wells <lois.wells@raspberrypi.org>
2022-08-11 12:35:53 +0100

    Getting the dropdown to close and grey background

--- src/components/Menus/Dropdown/Dropdown.js
+++ src/components/Menus/Dropdown/Dropdown.js
@@ -1,25 +1,43 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 import './Dropdown.scss'
 
 const Dropdown = (props) => {
   const {ButtonIcon, buttonText, menuOptions, MenuContent} = props
   const [isOpen, setOpen] = useState(false)
+  // const dropdown = useRef()
+  
+  // useEffect(() => {
+  //   /**
+  //    * Alert if clicked on outside of element
+  //    */
+  //   function handleClickOutside(event) {
+  //     if (dropdown.current && !dropdown.current.contains(event.target)) {
+  //       setOpen(false)
+  //     }
+  //   }
+  //   // Bind the event listener
+  //   document.addEventListener("mousedown", handleClickOutside);
+  //   return () => {
+  //     // Unbind the event listener on clean up
+  //     document.removeEventListener("mousedown", handleClickOutside);
+  //   };
+  // }, [dropdown]);
+
   return (
     <div className='dropdown'>
       <div className='dropdown-button' onClick={() => setOpen(!isOpen)}>
         <ButtonIcon />
         <span>{buttonText}</span>
       </div>
+      {isOpen ? <div className='dropdown-backdrop' onClick={() => setOpen(false)}></div> : null}
       {isOpen ?
-      MenuContent ? <MenuContent className='dropdown-container' /> :
+      MenuContent ? <MenuContent/> :
       <div className='dropdown-container'>
-        { MenuContent ? <MenuContent /> :
-        menuOptions ? menuOptions.map((option, i) => (
+        {menuOptions ? menuOptions.map((option, i) => (
           <p key={i}>{option}</p>
         )) : null }
-
-      </div> : null}
+      </div> : null }
     </div>
   )
 }
