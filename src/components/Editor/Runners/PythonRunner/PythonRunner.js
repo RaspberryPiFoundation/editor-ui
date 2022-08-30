44e5d45f7fce18c83974862fa4ced8907e5bf246
Lois Wells <lois.wells@raspberrypi.org>
2022-08-30 11:53:01 +0100

    Defaulting to visual tab if sense hat always enabled

--- src/components/Editor/Runners/PythonRunner/PythonRunner.js
+++ src/components/Editor/Runners/PythonRunner/PythonRunner.js
@@ -295,7 +295,7 @@ const PythonRunner = () => {
 
   return (
     <div className="pythonrunner-container">
-      <Tabs forceRenderTabPanel={true} defaultIndex={1}>
+      <Tabs forceRenderTabPanel={true} defaultIndex={senseHatAlwaysEnabled ? 0 : 1}>
         <TabList>
           <Tab key={0}>Visual Output</Tab>
           <Tab key={1}>Text Output</Tab>
