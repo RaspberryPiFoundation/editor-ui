7d411f535c2ebc63e149412b915faf710de96b54
Patrick Cherry <patch0@users.noreply.github.com>
2022-09-02 10:42:25 +0100

    Fix up eslint linting errors (#175)
    * Alter `stopping..` timeout test to work
    * Remove unused dependencies

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,7 +3,10 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+/* global globalThis */
 globalThis.IS_REACT_ACT_ENVIRONMENT = true;
+
 window.matchMedia = (query) => ({
   matches: false,
   media: query,
