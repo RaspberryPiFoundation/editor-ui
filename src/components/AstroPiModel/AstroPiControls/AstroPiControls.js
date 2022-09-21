d7ac49ae988a4ec58f7e18c2c0538fc9f330c209
Lois Wells <lois.wells@raspberrypi.org>
2022-09-21 12:34:39 +0100

    Moving fourth sense hat column below

--- src/components/AstroPiModel/AstroPiControls/AstroPiControls.js
+++ src/components/AstroPiModel/AstroPiControls/AstroPiControls.js
@@ -13,11 +13,13 @@ const AstroPiControls = (props) => {
     <div className='sense-hat-controls'>
       <h2 className='sense-hat-controls-heading'>Space Station Control Panel</h2>
       <div className="sense-hat-controls-panel">
+        <div className='sense-hat-controls-panel__sliders'>
           <SliderInput name="temperature" unit="°C" min={-40} max={120} defaultValue={temperature} Icon={TemperatureIcon} />
           <SliderInput name="pressure" unit="hPa" min={260} max={1260} defaultValue={pressure} Icon={PressureIcon}/>
           <SliderInput name="humidity" unit="%" min={0} max={100} defaultValue={humidity} Icon={HumidityIcon}/>
+        </div>
       
-        <div className="sense-hat-controls-panel__control">
+        <div className="sense-hat-controls-panel__control sense-hat-controls-panel__control-last">
           <Input name="colour" label="Colour" type="color" defaultValue={colour} />
           <MotionInput defaultValue={motion} />
           <Stopwatch />
