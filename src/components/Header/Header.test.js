11762aa2a4e04410d3542e74c9f7ca01b56c49e3
Lois Wells <lois.wells@raspberrypi.org>
2022-09-23 12:36:54 +0100

    tidying

--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -2,7 +2,6 @@ import React from "react";
 import { fireEvent, render } from "@testing-library/react"
 import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
-import { MemoryRouter } from "react-router-dom";
 import axios from "axios";
 
 import Header from "./Header";
