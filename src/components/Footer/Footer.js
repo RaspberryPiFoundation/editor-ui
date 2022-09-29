14113b89c6cf16da23df8f5d5d856b632728d25e
Lois Wells <lois.wells@raspberrypi.org>
2022-09-29 13:12:52 +0100

    Restyle the footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,10 +6,7 @@ import './Footer.scss';
 const Footer = () => {
   return (
     <footer className='editor-footer'>
-      <div className='editor-footer__foundation'>
-        <img src={rpf_logo} alt="Raspberry Pi Logo" />
-        <span className='editor-footer__foundation-name'>Raspberry Pi Foundation UK registered charity 1129409</span>
-      </div>
+      <span className='editor-footer__name'>Raspberry Pi Foundation UK registered charity 1129409</span>
       <div className='editor-footer__links'>
         <a className='editor-footer__links-link' href='https://www.raspberrypi.org/privacy'>Privacy</a>
         <a className='editor-footer__links-link' href='https://www.raspberrypi.org/cookies'>Cookies</a>
