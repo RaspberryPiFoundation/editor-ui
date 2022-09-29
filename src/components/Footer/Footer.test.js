d50fb104d5e8e659d279bb1d4b58880142c171e9
Lois Wells <lois.wells@raspberrypi.org>
2022-09-29 09:49:11 +0100

    Testing footer links

--- src/components/Footer/Footer.test.js
+++ src/components/Footer/Footer.test.js
@@ -3,8 +3,27 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import Footer from "./Footer";
 
-
 test('Footer renders', () => {
   render(<Footer/>)
   expect(screen.queryByText(/Raspberry Pi Foundation/)).toBeInTheDocument()
+})
+
+test('Links to privacy policy', () => {
+  render(<Footer/>)
+  expect(screen.queryByText('Privacy')).toHaveAttribute('href', 'https://www.raspberrypi.org/privacy')
+})
+
+test('Links to cookie policy', () => {
+  render(<Footer/>)
+  expect(screen.queryByText('Cookies')).toHaveAttribute('href', 'https://www.raspberrypi.org/cookies')
+})
+
+test('Links to accessibility policy', () => {
+  render(<Footer/>)
+  expect(screen.queryByText('Accessibility')).toHaveAttribute('href', 'https://www.raspberrypi.org/accessibility')
+})
+
+test('Links to safeguarding policy', () => {
+  render(<Footer/>)
+  expect(screen.queryByText('Safeguarding')).toHaveAttribute('href', 'https://www.raspberrypi.org/safeguarding')
 })
