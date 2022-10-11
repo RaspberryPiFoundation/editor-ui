aff5b39f3fdb2bbeff82e480b2093365eaddaa20
Lois Wells <lois.wells@raspberrypi.org>
2022-10-11 17:28:05 +0100

    initial potential solution for hiding cookiebot banner in embedded viewer

--- src/components/EmbeddedViewer/EmbeddedViewer.js
+++ src/components/EmbeddedViewer/EmbeddedViewer.js
@@ -14,6 +14,25 @@ const EmbeddedViewer = (props) => {
   useProject('python', projectIdentifier);
   useEmbeddedMode(true);
 
+  window.addEventListener(
+      "CookiebotOnDialogInit",
+      function (e) {
+        console.log('firing event to bypass cookie banner')
+          if (window.Cookiebot.consent.stamp == 0) {
+            console.log('setting custom consent')
+              window.Cookiebot.submitCustomConsent(!0, !0, !0);
+              window.addEventListener(
+                  "CookiebotOnLoad",
+                  function (e) {
+                      window.Cookiebot.deleteConsentCookie();
+                  },
+                  false
+              );
+          }
+      },
+      false
+  );
+
   return projectLoaded === true ? (
     <div className='embedded-viewer'>
       <Output />
