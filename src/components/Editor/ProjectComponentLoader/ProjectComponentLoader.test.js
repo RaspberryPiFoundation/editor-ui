fafab4217416d90e77d6f1381c68f12ad624d418
Lois Wells <lois.wells@raspberrypi.org>
2022-12-19 16:17:05 +0000

    Linting fix

--- src/components/Editor/ProjectComponentLoader/ProjectComponentLoader.test.js
+++ src/components/Editor/ProjectComponentLoader/ProjectComponentLoader.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, screen, waitFor } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
 import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
 import ProjectComponentLoader from "./ProjectComponentLoader";
