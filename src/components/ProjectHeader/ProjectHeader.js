5d99fa3e151e5adfbc66cbb160c5ca900a1a096d
Izzy Smillie <isabelle.smillie@raspberrypi.org>
2023-01-04 17:10:42 +0000

    Responsive fixes and mixin file added

--- src/components/ProjectHeader/ProjectHeader.js
+++ src/components/ProjectHeader/ProjectHeader.js
@@ -7,13 +7,15 @@ const ProjectHeader = (props) => {
 
   return (
     <header className='editor-project-header'>
-      <div className='editor-project-header__content'>
-        <h2>{t('projectHeader.subTitle')}</h2>
-        <h1 className='editor-project-header__title'>{t('projectHeader.title')}</h1>
-        <h3>{t('projectHeader.text')}</h3>
-      </div>
-      <div className='editor-project-header__action'>
-        {props.children}
+      <div className='editor-project-header__inner'>
+        <div className='editor-project-header__content'>
+          <h2>{t('projectHeader.subTitle')}</h2>
+          <h1 className='editor-project-header__title'>{t('projectHeader.title')}</h1>
+          <h3>{t('projectHeader.text')}</h3>
+        </div>
+        <div className='editor-project-header__action'>
+          {props.children}
+        </div>
       </div>
     </header>
   );
