3c468899f7dce6ee4ff5461f24252e515acdecc2
Lois Wells <lois.wells@raspberrypi.org>
2023-01-25 17:20:37 +0000

    fix your projects page bug

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -41,7 +41,7 @@ const Header = () => {
       <header className='editor-header'>
         <img className='editor-logo' src={editor_logo} alt={t('header.editorLogoAltText')}/>
         { user !== null ? (
-          <Link to='/projects' className='project-gallery-link'>
+          <Link to='/projects' className='project-gallery-link' reloadDocument>
             {<><HomeIcon />
             <span className='editor-header__text'>{t('header.projects')}</span></>}</Link>
         ) : null }
