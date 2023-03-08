c2c1500d844ac353c217ab1fb77fb1c732c28fb0
Izzy Smillie <isabelle.smillie@raspberrypi.org>
2023-03-08 16:48:26 +0000

    Set lang as en until we have ability to change it

--- src/i18n.js
+++ src/i18n.js
@@ -13,7 +13,7 @@ i18n
   .init({
     debug: true,
     fallbackLng: 'en',
-    whitelist: ['en'],
+    lng: 'en',
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
     },
