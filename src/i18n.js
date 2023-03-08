f1a65c9e16f8cbc60b5aaf1bb4599cf48f9d4831
Izzy Smillie <isabelle.smillie@raspberrypi.org>
2023-03-08 15:52:12 +0000

    Fix available locales

--- src/i18n.js
+++ src/i18n.js
@@ -12,7 +12,6 @@ i18n
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
     debug: true,
-    whitelist: ['en'],
     fallbackLng: 'en',
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
