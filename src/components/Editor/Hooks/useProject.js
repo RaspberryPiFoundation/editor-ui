3da47f15a83d1f31aac4c3e249cf5086516214cc
Lois Wells <lois.wells@raspberrypi.org>
2023-03-16 10:22:22 +0000

    tidying

--- src/components/Editor/Hooks/useProject.js
+++ src/components/Editor/Hooks/useProject.js
@@ -15,22 +15,18 @@ export const useProject = (projectIdentifier = null, locale = null, accessToken 
 
   useEffect(() => {
     if (locale === i18n.language) {
-      console.log('loading up project with locale', locale)
       var is_cached_saved_project = (projectIdentifier && cachedProject && cachedProject.identifier === projectIdentifier)
       var is_cached_unsaved_project = (!projectIdentifier && cachedProject)
 
       if (is_cached_saved_project || is_cached_unsaved_project) {
-        console.log('getting it from local storage')
         loadCachedProject()
         return
       }
 
       if (projectIdentifier) {
-        console.log('getting it from the database')
         dispatch(syncProject('load')({identifier: projectIdentifier, locale, accessToken}));
         return;
       }
-      console.log('using the default project')
       const data = defaultPythonProject;
       dispatch(setProject(data));
     }
