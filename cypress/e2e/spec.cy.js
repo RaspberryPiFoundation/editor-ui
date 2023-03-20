0dc85a756edfe7287cce7b752f517ddf50fc1ae3
Izzy Smillie <isabelle.smillie@raspberrypi.org>
2023-03-20 16:07:54 +0000

    Add run_sketch cypress test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -17,8 +17,16 @@ it("Interrupts py5 draws when stop button clicked", () => {
 
 it("runs with the magic comment present", () => {
   cy.visit(baseUrl)
-  cy.get("div[class=cm-content]").invoke('text', '# Py5: imported mode\nprint("hello world")')
+  cy.get("div[class=cm-content]").invoke('text', '# Py5: imported mode\nprint("hello world")\n')
   cy.get(".btn--run").click()
 
   cy.get(".pythonrunner-console-output-line").should("contain", "hello world")
+})
+
+it("Py5 imported mode runs without run_sketch()", () => {
+  cy.visit(baseUrl)
+  cy.get("div[class=cm-content]").invoke('text', '# Py5: imported mode\nimport py5\ndef draw():py5.rect(40,40,40)\n')
+  cy.get(".btn--run").click()
+
+  cy.get(".p5Canvas").should('be.visible')
 })
