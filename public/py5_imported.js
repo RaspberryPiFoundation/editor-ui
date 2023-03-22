8714181a8e10db4469c8112d81efbeb6570f69f9
Lois Wells <lois.wells@raspberrypi.org>
2023-03-22 16:31:07 +0000

    trying to get py5 imported mode working :/

--- public/py5_imported.js
+++ public/py5_imported.js
@@ -1,8 +1,22 @@
 const $builtinmodule = function(name) {
   var py5 = Sk.importModule('py5', false, false)
-  var mod = py5.$d
-  Sk.builtins.height = mod.height
-  Sk.builtins.width = mod.width
+  const mod = py5.$d
+  Sk.builtins.height = new Sk.builtin.int_(0);
+  Sk.builtins.width = new Sk.builtin.int_(0);
+
+  mod.size = new Sk.builtin.func(function (w, h, mode) {
+    if (typeof(mode) === "undefined") {
+      mode = mod.P2D;
+    }
+    mod.pInst.createCanvas(w.v, h.v, mode.v);
+    mod.width = new Sk.builtin.int_(mod.pInst.width);
+    mod.height = new Sk.builtin.int_(mod.pInst.height);
+    Sk.builtins.width = mod.width
+    Sk.builtins.height = mod.height
+    console.log(Sk.builtins)
+    mod.renderMode = mode;
+  });
+
   Sk.builtins.mouse_x = mod.mouse_x
   Sk.builtins.mouse_y = mod.mouse_y
   Sk.builtins.frame_count = mod.frame_count
