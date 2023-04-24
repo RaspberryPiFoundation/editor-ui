ebc225b1caca5cbbe30bcec028efb560c21821c5
Lois Wells <lois.wells@raspberrypi.org>
2023-04-24 15:02:00 +0100

    some minor tweaks

--- src/components/AstroPiModel/FlightCase.js
+++ src/components/AstroPiModel/FlightCase.js
@@ -1,26 +1,18 @@
-import * as THREE from 'three';
-
 import React from 'react';
-import { useLoader } from "@react-three/fiber";
-import { GLTFLoader } from 'three-stdlib/loaders/GLTFLoader.cjs.js';
-import { DRACOLoader } from 'three-stdlib/loaders/DRACOLoader.js';
-
+import * as THREE from 'three';
+import { useGLTF } from '@react-three/drei';
 import Sk from "skulpt";
 
 const FlightCase = () => {
 
-  const gltf = useLoader(GLTFLoader, `${process.env.PUBLIC_URL}/models/raspi-compressed.glb`, loader => {
-    const dracoLoader = new DRACOLoader();
-    dracoLoader.setDecoderPath( `${process.env.PUBLIC_URL}/three/examples/js/libs/draco/` );
-    loader.setDRACOLoader( dracoLoader );
-  })
-  window.mod=gltf.scene
+  const { scene } = useGLTF(`${process.env.PUBLIC_URL}/models/raspi-compressed.glb`)
+  window.mod=scene
 
   function setPixel(ledIndex,r,g,b) {
     var x = ledIndex % 8;
     var y = Math.floor(ledIndex / 8);
     var newMaterial = new THREE.MeshStandardMaterial({color: `rgb(${r},${g},${b})`});
-    var object = gltf.scene.getObjectByName(`circle${x}_${7-y}-1`);
+    var object = scene.getObjectByName(`circle${x}_${7-y}-1`);
 
     if(object != null){
       object.material = newMaterial;
@@ -61,7 +53,7 @@ const FlightCase = () => {
   }
 
   return (
-    <primitive object={gltf.scene} scale={4} />
+    <primitive object={scene} scale={4} />
   )
 }
 
