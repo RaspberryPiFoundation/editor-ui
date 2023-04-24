28e727b2a3238c848ff9f6eec50b99d0641fe81f
Lois Wells <lois.wells@raspberrypi.org>
2023-04-24 17:21:15 +0100

    Adding potential performance improvement

--- src/components/AstroPiModel/FlightCase.js
+++ src/components/AstroPiModel/FlightCase.js
@@ -1,56 +1,58 @@
 import React from 'react';
-// import * as THREE from 'three';
+import * as THREE from 'three';
 import { useGLTF } from '@react-three/drei';
-// import Sk from "skulpt";
+import Sk from "skulpt";
+import { invalidate } from '@react-three/fiber';
 
 const FlightCase = () => {
 
   const { scene } = useGLTF(`${process.env.PUBLIC_URL}/models/raspi-compressed.glb`)
   window.mod=scene
 
-  // function setPixel(ledIndex,r,g,b) {
-  //   var x = ledIndex % 8;
-  //   var y = Math.floor(ledIndex / 8);
-  //   var newMaterial = new THREE.MeshStandardMaterial({color: `rgb(${r},${g},${b})`});
-  //   var object = scene.getObjectByName(`circle${x}_${7-y}-1`);
+  function setPixel(ledIndex,r,g,b) {
+    var x = ledIndex % 8;
+    var y = Math.floor(ledIndex / 8);
+    var newMaterial = new THREE.MeshStandardMaterial({color: `rgb(${r},${g},${b})`});
+    var object = scene.getObjectByName(`circle${x}_${7-y}-1`);
 
-  //   if(object != null){
-  //     object.material = newMaterial;
-  //   }
-  // }
+    if(object != null){
+      object.material = newMaterial;
+    }
+  }
 
-  // function setPixels(indexes, pix) {
-  //   if(indexes == null){
-  //     indexes = Array.from(Array(8*8).keys())
-  //   }
+  function setPixels(indexes, pix) {
+    if(indexes == null){
+      indexes = Array.from(Array(8*8).keys())
+    }
 
-  //   var i = 0;
-  //   for (const ledIndex of indexes){
-  //     setPixel(ledIndex, pix[i][0], pix[i][1], pix[i][2])
-  //     i += 1;
-  //   }
-  // }
+    var i = 0;
+    for (const ledIndex of indexes){
+      setPixel(ledIndex, pix[i][0], pix[i][1], pix[i][2])
+      i += 1;
+    }
+  }
 
-  // Sk.sense_hat_emit = function(event, data) {
+  Sk.sense_hat_emit = function(event, data) {
 
-    // if (event && event === 'setpixel') {
-    //   // change the led
-    //   const ledIndex = data;
-    //   const ledData  = Sk.sense_hat.pixels[ledIndex];
+    if (event && event === 'setpixel') {
+      // change the led
+      const ledIndex = data;
+      const ledData  = Sk.sense_hat.pixels[ledIndex];
 
-    //   // Convert LED-RGB to RGB565 // and then to RGB555
-    //   Sk.sense_hat.pixels[ledIndex] = [
-    //     ledData[0] & ~7,
-    //     ledData[1] & ~3,
-    //     ledData[2] & ~7
-    //   ];
+      // Convert LED-RGB to RGB565 // and then to RGB555
+      Sk.sense_hat.pixels[ledIndex] = [
+        ledData[0] & ~7,
+        ledData[1] & ~3,
+        ledData[2] & ~7
+      ];
 
-    //   setPixel(ledIndex, parseInt(ledData[0]*255), parseInt(ledData[1]*255), parseInt(ledData[2]*255));
-    // }
-    // else if (event && event === 'setpixels') {
-    //   setPixels(data, Sk.sense_hat.pixels);
-    // }
-  // }
+      setPixel(ledIndex, parseInt(ledData[0]*255), parseInt(ledData[1]*255), parseInt(ledData[2]*255));
+    }
+    else if (event && event === 'setpixels') {
+      setPixels(data, Sk.sense_hat.pixels);
+    }
+    invalidate()
+  }
 
   return (
     <primitive object={scene} scale={4} />
