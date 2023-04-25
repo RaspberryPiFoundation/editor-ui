3577bc12fe0aacbf8186ae5ee5d3c36fc75e245c
Lois Wells <lois.wells@raspberrypi.org>
2023-04-25 12:06:48 +0100

    ditching performance monitor

--- src/components/AstroPiModel/Simulator.js
+++ src/components/AstroPiModel/Simulator.js
@@ -2,7 +2,7 @@ import React from 'react';
 import * as THREE from 'three';
 import { ResizeObserver } from "@juggle/resize-observer";
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls, PerspectiveCamera, PerformanceMonitor } from "@react-three/drei";
+import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
 import Lighting from './Lighting';
 import { Suspense } from 'react';
 
@@ -55,16 +55,6 @@ const Simulator = (props) => {
     }
   };
 
-  // const [dpr, setDpr] = useState(2)
-  // const increaseResolution = () => {
-  //   console.log('upping resolution')
-  //   setDpr(2)
-  // }
-  // const decreaseResolution = () => {
-  //   console.log('downing resolution')
-  //   setDpr(1)
-  // }
-
   return (
     <Canvas
       frameloop='demand'
@@ -73,16 +63,13 @@ const Simulator = (props) => {
       onPointerOut={handleDragStop}
       onPointerMove={dragModel}
       resize={{polyfill: ResizeObserver}}
-      dpr={0.25}
     >
-      <PerformanceMonitor>
         <Lighting />
         <Suspense fallback={null}>
           <PerspectiveCamera makeDefault fov={25} near={1} far={20000} position={[0, 1.5, 0]} />
           <FlightCase />
           <OrbitControls enableRotate = {false} enablePan = {false} enableZoom = {false} enabled = {false} />
         </Suspense>
-      </PerformanceMonitor>
     </Canvas>
   )
 };
