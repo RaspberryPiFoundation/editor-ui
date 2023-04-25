925feb8c659d2b6ebefabe58b114ebe8da8bb2fd
Lois Wells <lois.wells@raspberrypi.org>
2023-04-25 11:20:36 +0100

    Adding missing import

--- src/components/AstroPiModel/Simulator.js
+++ src/components/AstroPiModel/Simulator.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as THREE from 'three';
 import { ResizeObserver } from "@juggle/resize-observer";
 import { Canvas } from "@react-three/fiber";
