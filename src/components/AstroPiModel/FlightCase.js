ff884323669f20d29594c178a63cf84c5e8ef773
Lois Wells <lois.wells@raspberrypi.org>
2023-04-25 12:36:33 +0100

    putting led caching back in

--- src/components/AstroPiModel/FlightCase.js
+++ src/components/AstroPiModel/FlightCase.js
@@ -9,10 +9,21 @@ const FlightCase = () => {
   const { scene } = useGLTF(`${process.env.PUBLIC_URL}/models/raspi-compressed.glb`)
   window.mod = scene
 
+  const blankLED = new THREE.MeshStandardMaterial({color: `rgb(0,0,0)`})
+  var leds = {
+    '0_0_0': blankLED
+  }
+
   function setPixel(ledIndex,r,g,b) {
     var x = ledIndex % 8;
     var y = Math.floor(ledIndex / 8);
-    var ledMaterial = new THREE.MeshStandardMaterial({color: `rgb(${r},${g},${b})`})
+    let ledMaterial
+    if (`${r}_${g}_${b}` in leds) {
+      ledMaterial = leds[`${r}_${g}_${b}`]
+    } else {
+      ledMaterial = new THREE.MeshStandardMaterial({color: `rgb(${r},${g},${b})`});
+      leds[`${r}_${g}_${b}`] = ledMaterial
+    }
     var object = scene.getObjectByName(`circle${x}_${7-y}-1`);
 
     if(object != null){
