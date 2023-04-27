7c851ba5204df8de3f3dc4701431c5f05cec54f3
Lois Wells <lois.wells@raspberrypi.org>
2023-04-27 16:56:49 +0100

    tidying

--- src/Icons.js
+++ src/Icons.js
@@ -430,7 +430,8 @@ export const RunIcon = () => {
       height="14"
       viewBox="0 0 11 14"
       fill="none"
-      xmlns="http://www.w3.org/2000/svg">
+      xmlns="http://www.w3.org/2000/svg"
+    >
       <path d="M0 14V0L11 7L0 14Z"/>
     </svg>
   );
@@ -477,7 +478,8 @@ export const StopIcon = () => {
       height="12"
       viewBox="0 0 12 12"
       fill="none"
-      xmlns="http://www.w3.org/2000/svg">
+      xmlns="http://www.w3.org/2000/svg"
+    >
       <path d="M0 12V0H12V12H0Z"/>
     </svg>
   );
