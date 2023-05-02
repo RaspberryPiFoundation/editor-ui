59d10933456e769e6c84c893181b23dc5613f449
Scott <scott.adams@raspberrypi.org>
2023-05-02 19:36:30 +0100

    fix(linting): revert redundant formatting of unchanged files

--- src/components/Modals/NewFileModal.js
+++ src/components/Modals/NewFileModal.js
@@ -1,75 +1,61 @@
-import React, { useCallback } from "react";
-import Modal from "react-modal";
+import React, { useCallback } from 'react'
+import Modal from 'react-modal';
 
-import NameErrorMessage from "../Editor/ErrorMessage/NameErrorMessage";
-import { CloseIcon } from "../../Icons";
-import Button from "../Button/Button";
-import {
-  addProjectComponent,
-  closeNewFileModal,
-  openFile,
-} from "../Editor/EditorSlice";
-import { useDispatch, useSelector } from "react-redux";
-import { useTranslation } from "react-i18next";
-import { validateFileName } from "../../utils/componentNameValidation";
+import NameErrorMessage from '../Editor/ErrorMessage/NameErrorMessage'
+import { CloseIcon } from '../../Icons'
+import Button from '../Button/Button'
+import { addProjectComponent, closeNewFileModal, openFile } from '../Editor/EditorSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { useTranslation } from 'react-i18next';
+import { validateFileName } from '../../utils/componentNameValidation';
 
 const NewFileModal = () => {
-  const { t } = useTranslation();
-  const dispatch = useDispatch();
-  const projectType = useSelector((state) => state.editor.project.project_type);
-  const projectComponents = useSelector(
-    (state) => state.editor.project.components
-  );
-  const componentNames = projectComponents.map(
-    (component) => `${component.name}.${component.extension}`
-  );
 
-  const isModalOpen = useSelector((state) => state.editor.newFileModalShowing);
-  const closeModal = () => dispatch(closeNewFileModal());
+  const { t } = useTranslation()
+  const dispatch = useDispatch();
+  const projectType = useSelector((state) => state.editor.project.project_type)
+  const projectComponents = useSelector((state) => state.editor.project.components);
+  const componentNames = projectComponents.map(component => `${component.name}.${component.extension}`)
+
+  const isModalOpen = useSelector((state) => state.editor.newFileModalShowing)
+  const closeModal = () => dispatch(closeNewFileModal())
 
   const nameInput = useCallback((node) => {
     if (node) {
-      node.focus();
+      node.focus()
     }
-  }, []);
+  }, [])
 
   const onKeyDown = (e) => {
-    if (e.key === "Enter") {
-      createComponent();
+    if (e.key === 'Enter') {
+      createComponent()
     }
-  };
+  }
 
   const createComponent = () => {
-    const fileName = document.getElementById("name").value;
-    const name = fileName.split(".")[0];
-    const extension = fileName.split(".").slice(1).join(".");
-    console.log({ fileName, projectType, componentNames });
+    const fileName = document.getElementById('name').value
+    const name = fileName.split('.')[0];
+    const extension = fileName.split('.').slice(1).join('.');
     validateFileName(fileName, projectType, componentNames, dispatch, t, () => {
-      dispatch(addProjectComponent({ extension: extension, name: name }));
-      dispatch(openFile(fileName));
+      dispatch(addProjectComponent({extension: extension, name: name}));
+      dispatch(openFile(fileName))
       closeModal();
-    });
-  };
+    })
+  }
 
   return (
     <Modal
       isOpen={isModalOpen}
       onRequestClose={closeModal}
       contentLabel="New File"
-      className="modal-content"
-      overlayClassName="modal-overlay"
-      parentSelector={() => document.querySelector("#app")}
-      appElement={document.getElementById("app") || undefined}
+      className='modal-content'
+      overlayClassName='modal-overlay'
+      parentSelector={() => document.querySelector('#app')}
+      appElement={document.getElementById('app') || undefined}
     >
-      <div className="modal-content__header">
-        <h2 className="modal-content__heading">
-          {t("filePane.newFileModal.heading")}
-        </h2>
-        <Button
-          className="btn--tertiary"
-          onClickHandler={closeModal}
-          ButtonIcon={CloseIcon}
-        />
+      <div className='modal-content__header'>
+        <h2 className='modal-content__heading'>{t('filePane.newFileModal.heading')}</h2>
+        <Button className='btn--tertiary' onClickHandler={closeModal} ButtonIcon = {CloseIcon} />
       </div>
 
       <div className='modal-content__body'>
@@ -79,13 +65,7 @@ const NewFileModal = () => {
         </div>
         <div>
           <NameErrorMessage />
-          <input
-            ref={nameInput}
-            type="text"
-            name="name"
-            id="name"
-            onKeyDown={onKeyDown}
-          ></input>
+          <input ref={nameInput} type='text' name='name' id='name' onKeyDown={onKeyDown}></input>
         </div>
       </div>
 
@@ -94,7 +74,7 @@ const NewFileModal = () => {
         <Button className='btn--secondary' buttonText={t('filePane.newFileModal.cancel')} onClickHandler={closeModal} />
       </div>
     </Modal>
-  );
-};
+  )
+}
 
-export default NewFileModal;
+export default NewFileModal
--- src/utils/componentNameValidation.js
+++ src/utils/componentNameValidation.js
@@ -15,55 +15,34 @@ const allowedExtensions = {
 const allowedExtensionsString = (projectType, t) => {
   const extensionsList = allowedExtensions[projectType];
   if (extensionsList.length === 1) {
-    return `'.${extensionsList[0]}'`;
+    return `'.${extensionsList[0]}'`
   } else {
-    return `'.${extensionsList.slice(0, -1).join(`', '.`)}' ${t(
-      "filePane.errors.or"
-    )} '.${extensionsList[extensionsList.length - 1]}'`;
+    return `'.${extensionsList.slice(0,-1).join(`', '.`)}' ${t('filePane.errors.or')} '.${extensionsList[extensionsList.length-1]}'`;
   }
-};
+}
 
 const isValidFileName = (fileName, projectType, componentNames) => {
-  const extension = fileName.split(".").slice(1).join(".");
-  if (
-    allowedExtensions[projectType].includes(extension) &&
-    !componentNames.includes(fileName) &&
-    fileName.split(" ").length === 1
-  ) {
+  const extension = fileName.split('.').slice(1).join('.')
+  if (allowedExtensions[projectType].includes(extension) && !componentNames.includes(fileName) && fileName.split(' ').length === 1) {
     return true;
   } else {
     return false;
   }
-};
+}
 
-export const validateFileName = (
-  fileName,
-  projectType = "python",
-  componentNames,
-  dispatch,
-  t,
-  callback,
-  currentFileName = null
-) => {
-  const extension = fileName.split(".").slice(1).join(".");
-  if (
-    isValidFileName(fileName, projectType, componentNames) ||
-    (currentFileName && fileName === currentFileName)
-  ) {
-    callback();
+export const validateFileName = (fileName, projectType="python", componentNames, dispatch, t, callback, currentFileName=null) => {
+  const extension = fileName.split('.').slice(1).join('.');
+  if (isValidFileName(fileName, projectType, componentNames) || (currentFileName && fileName === currentFileName)) {
+    callback()
   } else if (componentNames.includes(fileName)) {
-    dispatch(setNameError(t("filePane.errors.notUnique")));
-  } else if (fileName.split(" ").length > 1) {
-    dispatch(setNameError(t("filePane.errors.containsSpaces")));
+    dispatch(setNameError(t('filePane.errors.notUnique')));
+  } else if (fileName.split(' ').length > 1) {
+    dispatch(setNameError(t('filePane.errors.containsSpaces')))
   } else if (!allowedExtensions[projectType].includes(extension)) {
-    dispatch(
-      setNameError(
-        t("filePane.errors.unsupportedExtension", {
-          allowedExtensions: allowedExtensionsString(projectType, t),
-        })
-      )
-    );
+    dispatch(setNameError(t('filePane.errors.unsupportedExtension', {allowedExtensions: allowedExtensionsString(projectType, t)})));
   } else {
-    dispatch(setNameError(t("filePane.errors.generalError")));
+    dispatch(setNameError(t('filePane.errors.generalError')));
   }
-};
+}
+
+
