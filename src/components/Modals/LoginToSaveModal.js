99af04d4f0a8814252f018d1c40d1bf6976ad57a
Lois Wells <lois.wells@raspberrypi.org>
2023-05-05 16:39:23 +0100

    refactor login to save and 404 modals

--- src/components/Modals/LoginToSaveModal.js
+++ src/components/Modals/LoginToSaveModal.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Modal from 'react-modal';
 import { useDispatch, useSelector } from "react-redux";
 import { useTranslation } from "react-i18next";
 
@@ -7,8 +6,8 @@ import { closeLoginToSaveModal } from "../Editor/EditorSlice";
 import DownloadButton from "../Header/DownloadButton";
 import LoginButton from "../Login/LoginButton";
 import '../../Modal.scss';
-import { CloseIcon } from "../../Icons";
 import Button from "../Button/Button";
+import GeneralModal from "./GeneralModal";
 
 const LoginToSaveModal = () => {
   const dispatch = useDispatch()
@@ -18,34 +17,22 @@ const LoginToSaveModal = () => {
   const closeModal = () => dispatch(closeLoginToSaveModal());
 
   return (
-    <>
-      <Modal
-        isOpen={isModalOpen}
-        onRequestClose={closeModal}
-        className='modal-content'
-        overlayClassName='modal-overlay'
-        contentLabel="Login to Save"
-        parentSelector={() => document.querySelector('#app')}
-        appElement={document.getElementById('app') || undefined}
-      >
-        <div className='modal-content__header'>
-          <h2 className='modal-content__heading'>{t('loginToSaveModal.heading')}</h2>
-          <Button className='btn--tertiary' onClickHandler={closeModal} ButtonIcon = {CloseIcon} />
-        </div>
-
-        <div className='modal-content__body'>
-          <p className='modal-content__text'>{t('loginToSaveModal.loginText')}</p>
-          <p className='modal-content__text'>{t('loginToSaveModal.downloadText')}</p>
-        </div>
-
-        <div className='modal-content__buttons' >
-          <LoginButton className='btn--primary' buttonText={t('loginToSaveModal.loginButtonText')} triggerSave />
-          <DownloadButton buttonText = {t('loginToSaveModal.downloadButtonText')} className = 'btn--secondary' />
-          <Button buttonText = {t('loginToSaveModal.cancel')} className='btn--tertiary' onClickHandler={closeModal}/>
-        </div>
-        
-      </Modal>
-    </>
+    <GeneralModal
+      isOpen={isModalOpen}
+      closeModal={closeModal}
+      withCloseButton
+      contentLabel={t('loginToSaveModal.heading')}
+      heading={t('loginToSaveModal.heading')}
+      text={[
+        {type: 'paragraph', content: t('loginToSaveModal.loginText')},
+        {type: 'paragraph', content: t('loginToSaveModal.downloadText')}
+      ]}
+      buttons={[
+        <LoginButton className='btn--primary' buttonText={t('loginToSaveModal.loginButtonText')} triggerSave />,
+        <DownloadButton buttonText = {t('loginToSaveModal.downloadButtonText')} className = 'btn--secondary' />,
+        <Button buttonText = {t('loginToSaveModal.cancel')} className='btn--tertiary' onClickHandler={closeModal}/>
+      ]}
+    />
   );
 }
 
--- src/components/Modals/NotFoundModal.js
+++ src/components/Modals/NotFoundModal.js
@@ -1,13 +1,12 @@
 import React from "react";
-import Modal from 'react-modal';
 import { useDispatch, useSelector } from "react-redux";
 import { useTranslation } from "react-i18next";
 
 import Button from "../Button/Button";
 import '../../Modal.scss';
 import { closeNotFoundModal, syncProject } from "../Editor/EditorSlice";
-import { CloseIcon } from "../../Icons";
 import { defaultPythonProject } from "../../utils/defaultProjects";
+import GeneralModal from "./GeneralModal";
 
 const NotFoundModal = () => {
   const dispatch = useDispatch()
@@ -25,31 +24,20 @@ const NotFoundModal = () => {
   }
 
   return (
-    <>
-      <Modal
-        isOpen={isModalOpen}
-        onRequestClose={closeModal}
-        className='modal-content'
-        overlayClassName='modal-overlay'
-        contentLabel={t('project.notFoundModal.heading')}
-        parentSelector={() => document.querySelector('#app')}
-        appElement={document.getElementById('app') || undefined}
-      >
-        <div className='modal-content__header'>
-          <h2 className='modal-content__heading'>{t('project.notFoundModal.heading')}</h2>
-          <Button className='btn--tertiary' onClickHandler={closeModal} ButtonIcon = {CloseIcon} />
-        </div>
-
-        <div className='modal-content__body'>
-          <p className='modal-content__text'>{t('project.notFoundModal.text')}</p>
-        </div>
-
-        <div className='modal-content__buttons' >
-          <Button className='btn--primary' buttonText={t('project.notFoundModal.newProject')} onClickHandler={createNewProject} />
-          <a className='btn btn--secondary' href='https://projects.raspberrypi.org'>{t('project.notFoundModal.projectsSiteLinkText')}</a>
-        </div>
-      </Modal>
-    </>
+    <GeneralModal
+      isOpen={isModalOpen}
+      closeModal={closeModal}
+      withCloseButton
+      contentLabel={t('project.notFoundModal.heading')}
+      heading={t('project.notFoundModal.heading')}
+      text={[
+        {type: 'paragraph', content: t('project.notFoundModal.text')}
+      ]}
+      buttons={[
+        <Button className='btn--primary' buttonText={t('project.notFoundModal.newProject')} onClickHandler={createNewProject} />,
+        <a className='btn btn--secondary' href='https://projects.raspberrypi.org'>{t('project.notFoundModal.projectsSiteLinkText')}</a>
+      ]}
+    />
   );
 }
 
