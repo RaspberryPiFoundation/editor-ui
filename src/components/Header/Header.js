7fb8a1eee2ab462235b8cd5ac4f637d0ed9e283c
Magdalena Jadach <magdalena@raspberrypi.org>
2023-05-09 13:18:15 +0100

    Icon type and size

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,7 +8,9 @@ import { syncProject, showLoginToSaveModal } from '../Editor/EditorSlice';
 import Dropdown from '../Menus/Dropdown/Dropdown';
 import SettingsMenu from '../Menus/SettingsMenu/SettingsMenu';
 import ProjectName from './ProjectName';
-import editor_logo from '../../assets/editor_logo.svg'
+// import editor_logo from '../../assets/editor_logo.svg'
+import htmlLogo from '../../assets/html_icon.svg'
+import pythonLogo from '../../assets/python_icon.svg'
 import DownloadButton from './DownloadButton';
 import { isOwner } from '../../utils/projectHelpers'
 import { Link } from 'react-router-dom';
@@ -19,6 +21,7 @@ const Header = () => {
   
   const user = useSelector((state) => state.auth.user)
   const project = useSelector((state) => state.editor.project)
+  const type = useSelector((state) => state.editor.project.project_type)
   const loading = useSelector((state) => state.editor.loading)
   const saving = useSelector((state) => state.editor.saving)
   const lastSavedTime = useSelector((state) => state.editor.lastSavedTime)
@@ -39,7 +42,7 @@ const Header = () => {
   return loading === 'success' && (
     <div className='editor-header-wrapper'>
       <header className='editor-header'>
-        <img className='editor-logo' src={editor_logo} alt={t('header.editorLogoAltText')}/>
+        <img className='editor-logo' src={type == 'python' ? pythonLogo : htmlLogo } alt={t('header.editorLogoAltText')}/>
         { user !== null ? (
           <Link to={`${locale}/projects`} className='project-gallery-link' reloadDocument>
             {<><HomeIcon />
