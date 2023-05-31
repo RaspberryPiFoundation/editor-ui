831af2291b943011d645e9c4551dcf69d23f22fa
Magdalena Jadach <magdalena@raspberrypi.org>
2023-05-31 15:14:33 +0100

    Basic styles

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -2,17 +2,27 @@ import React from "react";
 import { useTranslation } from "react-i18next";
 import "./LandingPage.scss";
 
+import startIcon from "../../assets/start_icon.svg";
 const LandingPage = () => {
   const { t } = useTranslation();
   return (
     <div className="landing-page-wrapper">
-      <h1>{t("landingPage.title")}</h1>
-      <h2>{t("landingPage.subtitle")}</h2>
-      <button>Python</button>
-      <button>Html</button>
-      <p>{t("landingPage.login")}</p>
-      <p>{t("landingPage.start")}</p>
-      <p>{t("landingPage.projects")}</p>
+      <div className="landing-page__projects">
+        <h1 className="landing-page__projects--title">
+          {t("landingPage.title")}
+        </h1>
+        <h2 className="landing-page__projects--subtitle">
+          {t("landingPage.subtitle")}
+        </h2>
+        <button className="btn btn--primary">{t("landingPage.python")}</button>
+        <button className="btn btn--primary">{t("landingPage.html")}</button>
+        <p>{t("landingPage.login")}</p>
+      </div>
+      <div className="landing-page__paths">
+        <p>{t("landingPage.start")}</p>
+        <p>{t("landingPage.projects")}</p>
+        <img className="" src={startIcon} alt={""} />
+      </div>
     </div>
   );
 };
