02655de7237c55003df8765e6e4b5cb15dceaa4d
Lois Wells <lois.wells@raspberrypi.org>
2023-06-14 10:00:50 +0100

    Output test

--- src/components/Editor/Output/Output.test.js
+++ src/components/Editor/Output/Output.test.js
@@ -1,10 +1,27 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { Provider } from "react-redux";
 import configureStore from "redux-mock-store";
 
 import Output from "./Output";
 import { MemoryRouter } from "react-router-dom";
+
+let mockBrowserPreview = "false";
+
+// jest.mock("react-router-dom", () => ({
+//   ...jest.requireActual("react-router-dom"),
+//   useSearchParams: () => [
+//     {
+//       get: (key) => (key === "browserPreview" ? mockBrowserPreview : null),
+//     },
+//   ],
+// }));
+
+jest
+  .spyOn(URLSearchParams.prototype, "get")
+  .mockImplementation((key) =>
+    key === "browserPreview" ? mockBrowserPreview : null,
+  );
 
 test("Component renders", () => {
   const middlewares = [];
@@ -25,4 +42,46 @@ test("Component renders", () => {
     </Provider>,
   );
   expect(container.lastChild).toHaveClass("proj-runner-container");
+});
+
+describe("When embedded", () => {
+  let store;
+
+  beforeEach(() => {
+    const middlewares = [];
+    const mockStore = configureStore(middlewares);
+    const initialState = {
+      editor: {
+        project: {
+          components: [],
+        },
+        isEmbedded: true,
+      },
+    };
+    store = mockStore(initialState);
+  });
+
+  test("Shows run bar when not browser preview", () => {
+    render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <Output />
+        </MemoryRouter>
+      </Provider>,
+    );
+    expect(screen.queryByText("runButton.run")).toBeInTheDocument();
+  });
+
+  // TODO: Get this test working
+  // test("Does not show run bar when browser preview", () => {
+  //   mockBrowserPreview = "true";
+  //   render(
+  //     <Provider store={store}>
+  //       <MemoryRouter>
+  //         <Output />
+  //       </MemoryRouter>
+  //     </Provider>,
+  //   );
+  //   expect(screen.queryByText("runButton.run")).not.toBeInTheDocument();
+  // });
 });
