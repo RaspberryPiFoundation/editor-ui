a324373021a6071223b52f65a9cd3c2f1bb820df
Magdalena Jadach <magdalena@raspberrypi.org>
2023-07-12 08:16:42 +0100

    Style the info section

--- src/components/Menus/Sidebar/InfoPanel/InfoPanel.js
+++ src/components/Menus/Sidebar/InfoPanel/InfoPanel.js
@@ -1,23 +1,34 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 import SidebarPanel from "../SidebarPanel";
 import { Link } from "react-router-dom";
 
-import { useTranslation } from "react-i18next";
+import "./InfoPanel.scss";
 
 const InfoPanel = () => {
   const { t } = useTranslation();
 
   return (
     <SidebarPanel heading={t("infoPanel.info")}>
-      <div className="editor-info ">
+      <div className="info-panel">
         <p>{t("sidebar.information_text")}</p>
       </div>
-      <div className="editor-info">
-        <Link className="editor-info__link">{t("sidebar.feedback")}</Link>
-        <Link className="editor-info__link">{t("sidebar.privacy")}</Link>
-        <Link className="editor-info__link">{t("sidebar.cookies")}</Link>
-        <Link className="editor-info__link">{t("sidebar.accessibility")}</Link>
-        <Link className="editor-info__link">{t("sidebar.safeguarding")}</Link>
+      <div className="info-panel info-panel__links">
+        <Link className="info-panel__link" to="/">
+          {t("sidebar.feedback")}
+        </Link>
+        <Link className="info-panel__link" to="/">
+          {t("sidebar.privacy")}
+        </Link>
+        <Link className="info-panel__link" to="/">
+          {t("sidebar.cookies")}
+        </Link>
+        <Link className="info-panel__link" to="/">
+          {t("sidebar.accessibility")}
+        </Link>
+        <Link className="info-panel__link" to="/">
+          {t("sidebar.safeguarding")}
+        </Link>
 
         <p>{t("sidebar.charity")}</p>
       </div>
