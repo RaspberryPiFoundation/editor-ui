fcb1ab752ae1ef4e4a2dc177e8c4789ab7df5be6
Magdalena Jadach <magdalena@raspberrypi.org>
2023-08-07 16:49:35 +0100

    Pull in a name from the state

--- src/components/ProjectStatus/ProjectStatus.js
+++ src/components/ProjectStatus/ProjectStatus.js
@@ -1,20 +1,24 @@
 import "./ProjectStatus.scss";
 import SaveButton from "../SaveButton/SaveButton";
-import ProjectName from "../ProjectName/ProjectName";
-import { useSelector } from "react-redux";
+import { useSelector, shallowEqual } from "react-redux";
+import React, { useRef } from "react";
 
 const ProjectStatus = () => {
+  const project = useSelector((state) => state.editor.project, shallowEqual);
   const lastSavedTime = useSelector((state) => state.editor.lastSavedTime);
   const user = useSelector((state) => state.auth.user);
   const saving = useSelector((state) => state.editor.saving);
   const loading = useSelector((state) => state.editor.loading);
+
   return (
     loading === "success" && (
       <div className="project-status">
         {lastSavedTime && user ? (
           <SaveButton saving={saving} lastSavedTime={lastSavedTime} />
         ) : null}
-        <ProjectName />
+        <div className="">
+          <p>{project.name}</p>
+        </div>
       </div>
     )
   );
