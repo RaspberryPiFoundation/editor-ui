bd0b5c586f935729176f67e606b1eac7b0756a9b
Magdalena Jadach <magdalena@raspberrypi.org>
2023-08-11 15:18:25 +0100

    Fix the test

--- src/components/Mobile/MobileProjectBar/MobileProjectBar.js
+++ src/components/Mobile/MobileProjectBar/MobileProjectBar.js
@@ -6,12 +6,11 @@ import React from "react";
 const MobileProjectBar = () => {
   const projectName = useSelector((state) => state.editor.project.name);
   const lastSavedTime = useSelector((state) => state.editor.lastSavedTime);
-  const user = useSelector((state) => state.auth.user);
 
   return (
     <div className="mobile-project-bar">
       <p className="mobile-project-bar__name">{projectName}</p>
-      {lastSavedTime && user ? <SaveStatus /> : null}
+      {lastSavedTime ? <SaveStatus /> : null}
     </div>
   );
 };
