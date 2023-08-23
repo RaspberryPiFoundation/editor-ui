aba60f4861aa3317af10c54e36f40f36ad1a2fa1
Scott <scott.adams@raspberrypi.org>
2023-08-23 14:05:11 +0100

    feat(Project,-MobileProject-&-ProjectComponentLoader): refactor to share modal states and actions between Project and MobileProject views

--- src/components/Mobile/MobileProject/MobileProject.js
+++ src/components/Mobile/MobileProject/MobileProject.js
@@ -9,12 +9,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { CodeIcon, PreviewIcon, MenuIcon } from "../../../Icons";
 import { useTranslation } from "react-i18next";
 import Sidebar from "../../Menus/Sidebar/Sidebar";
-import {
-  expireJustLoaded,
-  syncProject,
-  showSidebar,
-} from "../../Editor/EditorSlice";
-import { isOwner } from "../../../utils/projectHelpers";
+import { showSidebar } from "../../Editor/EditorSlice";
 
 const MobileProject = () => {
   const projectType = useSelector((state) => state.editor.project.project_type);
@@ -34,98 +29,52 @@ const MobileProject = () => {
     }
   }, [codeRunTriggered]);
 
-  // TODO: copy/paste autosave functionality from project code needs centralising or refactor
-  const user = useSelector((state) => state.auth.user);
-  const project = useSelector((state) => state.editor.project);
-  const justLoaded = useSelector((state) => state.editor.justLoaded);
-
-  useEffect(() => {
-    if (user && localStorage.getItem("awaitingSave")) {
-      if (isOwner(user, project)) {
-        dispatch(
-          syncProject("save")({
-            project,
-            accessToken: user.access_token,
-            autosave: false,
-          }),
-        );
-      } else if (user && project.identifier) {
-        dispatch(
-          syncProject("remix")({ project, accessToken: user.access_token }),
-        );
-      }
-      localStorage.removeItem("awaitingSave");
-      return;
-    }
-    let debouncer = setTimeout(() => {
-      if (isOwner(user, project) && project.identifier) {
-        if (justLoaded) {
-          dispatch(expireJustLoaded());
-        }
-        dispatch(
-          syncProject("save")({
-            project,
-            accessToken: user.access_token,
-            autosave: true,
-          }),
-        );
-      } else {
-        if (justLoaded) {
-          dispatch(expireJustLoaded());
-        } else {
-          localStorage.setItem(
-            project.identifier || "project",
-            JSON.stringify(project),
-          );
-        }
-      }
-    }, 2000);
-
-    return () => clearTimeout(debouncer);
-  }, [dispatch, project, user, justLoaded]);
-
-  return sidebarShowing ? (
-    <Sidebar />
-  ) : (
-    <div className="proj-container proj-editor-container proj-container--mobile">
-      <Tabs
-        forceRenderTabPanel={true}
-        selectedIndex={selectedTab}
-        onSelect={(index) => setSelectedTab(index)}
-      >
-        <TabPanel>
-          <EditorInput />
-        </TabPanel>
-        <TabPanel>
-          <Output />
-        </TabPanel>
-        <MobileProjectBar />
-        <div className="react-tabs__tab-container mobile-nav">
-          <span
-            className="react-tabs__tab-inner mobile-nav__menu"
-            onClick={openSidebar}
+  return (
+    <>
+      {sidebarShowing ? (
+        <Sidebar />
+      ) : (
+        <div className="proj-container proj-editor-container proj-container--mobile">
+          <Tabs
+            forceRenderTabPanel={true}
+            selectedIndex={selectedTab}
+            onSelect={(index) => setSelectedTab(index)}
           >
-            <MenuIcon />
-          </span>
-          <TabList>
-            <Tab>
-              <span className="react-tabs__tab-inner">
-                <CodeIcon />
-                {t("mobile.code")}
+            <TabPanel>
+              <EditorInput />
+            </TabPanel>
+            <TabPanel>
+              <Output />
+            </TabPanel>
+            <MobileProjectBar />
+            <div className="react-tabs__tab-container mobile-nav">
+              <span
+                className="react-tabs__tab-inner mobile-nav__menu"
+                onClick={openSidebar}
+              >
+                <MenuIcon />
               </span>
-            </Tab>
-            <Tab>
-              <span className="react-tabs__tab-inner">
-                <PreviewIcon />
-                {projectType === "html"
-                  ? t("mobile.preview")
-                  : t("mobile.output")}
-              </span>
-            </Tab>
-          </TabList>
+              <TabList>
+                <Tab>
+                  <span className="react-tabs__tab-inner">
+                    <CodeIcon />
+                    {t("mobile.code")}
+                  </span>
+                </Tab>
+                <Tab>
+                  <span className="react-tabs__tab-inner">
+                    <PreviewIcon />
+                    {projectType === "html"
+                      ? t("mobile.preview")
+                      : t("mobile.output")}
+                  </span>
+                </Tab>
+              </TabList>
+            </div>
+          </Tabs>
         </div>
-      </Tabs>
-    </div>
+      )}
+    </>
   );
 };
 
