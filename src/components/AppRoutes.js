036e83d470d8ac497edf71eeca0cba8239ad2d8a
Pete Simonovic <69108995+PetarSimonovic@users.noreply.github.com>
2023-09-01 09:10:06 +0100

    627: scss changes for scrolling

--- src/components/AppRoutes.js
+++ src/components/AppRoutes.js
@@ -29,7 +29,7 @@ const AppRoutes = () => (
 
     <Route path="/auth/silent_renew" element={<SilentRenew />} />
     <Route path={":locale"} element={<LocaleLayout />}>
-      <Route index element={<LandingPage />} />
+      <Route index element={<ProjectComponentLoader />} />
       <Route path={"projects"} element={<ProjectIndex />} />
       <Route
         path={"projects/:identifier"}
