8ce1d7067b870c0a806a1014af491062905f1126
Scott <scott.adams@raspberrypi.org>
2023-10-10 10:33:10 +0100

    remove suspense and lint

--- src/components/AppRoutes.jsx
+++ src/components/AppRoutes.jsx
@@ -1,27 +1,27 @@
-import React, { lazy, Suspense } from "react";
+import React, { lazy } from "react";
 import { Route, Routes, Navigate, useParams } from "react-router-dom";
 import * as Sentry from "@sentry/react";
 
 const Callback = lazy(() => import(/* webpackPrefetch: true */ "./Callback"));
 const SilentRenew = lazy(() =>
-  import(/* webpackPrefetch: true */ "./SilentRenew")
+  import(/* webpackPrefetch: true */ "./SilentRenew"),
 );
 const LocaleLayout = lazy(() =>
-  import(/* webpackPrefetch: true */ "./LocaleLayout/LocaleLayout")
+  import(/* webpackPrefetch: true */ "./LocaleLayout/LocaleLayout"),
 );
 const LandingPage = lazy(() =>
-  import(/* webpackPrefetch: true */ "./LandingPage/LandingPage")
+  import(/* webpackPrefetch: true */ "./LandingPage/LandingPage"),
 );
 const ProjectIndex = lazy(() =>
-  import(/* webpackPrefetch: true */ "./ProjectIndex/ProjectIndex")
+  import(/* webpackPrefetch: true */ "./ProjectIndex/ProjectIndex"),
 );
 const ProjectComponentLoader = lazy(() =>
   import(
     /* webpackPrefetch: true */ "./Editor/ProjectComponentLoader/ProjectComponentLoader"
-  )
+  ),
 );
 const EmbeddedViewer = lazy(() =>
-  import(/* webpackPrefetch: true */ "./EmbeddedViewer/EmbeddedViewer")
+  import(/* webpackPrefetch: true */ "./EmbeddedViewer/EmbeddedViewer"),
 );
 
 const projectLinkRedirects = [
@@ -39,41 +39,39 @@ const SentryRoutes = Sentry.withSentryReactRouterV6Routing(Routes);
 
 const AppRoutes = () => (
   <SentryRoutes>
-    <Suspense fallback={<></>}>
-      <Route path="/auth/callback" element={<Callback />} />
+    <Route path="/auth/callback" element={<Callback />} />
 
-      <Route path="/auth/silent_renew" element={<SilentRenew />} />
-      <Route path={":locale"} element={<LocaleLayout />}>
-        <Route index element={<LandingPage />} />
-        <Route path={"projects"} element={<ProjectIndex />} />
-        <Route
-          path={"projects/:identifier"}
-          element={<ProjectComponentLoader />}
-        />
-        <Route path="embed/viewer/:identifier" element={<EmbeddedViewer />} />
-      </Route>
-
+    <Route path="/auth/silent_renew" element={<SilentRenew />} />
+    <Route path={":locale"} element={<LocaleLayout />}>
+      <Route index element={<LandingPage />} />
+      <Route path={"projects"} element={<ProjectIndex />} />
       <Route
-        path="/embedded/projects/:identifier"
-        element={<ProjectComponentLoader embedded={true} />}
+        path={"projects/:identifier"}
+        element={<ProjectComponentLoader />}
       />
+      <Route path="embed/viewer/:identifier" element={<EmbeddedViewer />} />
+    </Route>
 
-      {/* Redirects will be moved into a cloudflare worker. This is just interim */}
+    <Route
+      path="/embedded/projects/:identifier"
+      element={<ProjectComponentLoader embedded={true} />}
+    />
 
-      {projectLinkRedirects.map((link) => {
-        return <Route key={link} path={link} element={<ProjectsRedirect />} />;
-      })}
+    {/* Redirects will be moved into a cloudflare worker. This is just interim */}
 
-      {localeRedirects.map((link) => {
-        return (
-          <Route
-            key={link}
-            path={link}
-            element={<Navigate replace to={`/en${link}`} />}
-          />
-        );
-      })}
-    </Suspense>
+    {projectLinkRedirects.map((link) => {
+      return <Route key={link} path={link} element={<ProjectsRedirect />} />;
+    })}
+
+    {localeRedirects.map((link) => {
+      return (
+        <Route
+          key={link}
+          path={link}
+          element={<Navigate replace to={`/en${link}`} />}
+        />
+      );
+    })}
   </SentryRoutes>
 );
 
