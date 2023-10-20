744d4376e44346776a228be282a974e633900fcb
Scott <scott.adams@raspberrypi.org>
2023-10-20 16:29:59 +0100

    fix(logoutbutton): fix order of events so hydra and local sessions are wiped

--- src/components/Login/LogoutButton.jsx
+++ src/components/Login/LogoutButton.jsx
@@ -2,19 +2,16 @@ import React from "react";
 import userManager from "../../utils/userManager";
 import { useTranslation } from "react-i18next";
 import Button from "../Button/Button";
-import { useNavigate } from "react-router-dom";
 import PropTypes from "prop-types";
 
 const LogoutButton = ({ className, user }) => {
   const { t } = useTranslation();
-  const navigate = useNavigate();
 
   const onLogoutButtonClick = async (event) => {
     event.preventDefault();
     userManager.signoutRedirect({ id_token_hint: user?.id_token });
     await userManager.removeUser();
     localStorage.clear();
-    navigate("/");
   };
 
   return (
