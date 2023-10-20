6f3483873b021ab8aaee138295f859a846e4579c
Scott <scott.adams@raspberrypi.org>
2023-10-20 15:56:07 +0100

    feat(oidc-redux-signout): hit signoutRedirect with a id_token during logout to invalidate hydra session

--- src/components/Login/LogoutButton.jsx
+++ src/components/Login/LogoutButton.jsx
@@ -3,14 +3,15 @@ import userManager from "../../utils/userManager";
 import { useTranslation } from "react-i18next";
 import Button from "../Button/Button";
 import { useNavigate } from "react-router-dom";
+import PropTypes from "prop-types";
 
-const LogoutButton = (props) => {
-  const { className } = props;
+const LogoutButton = ({ className, user }) => {
   const { t } = useTranslation();
   const navigate = useNavigate();
 
   const onLogoutButtonClick = async (event) => {
     event.preventDefault();
+    userManager.signoutRedirect({ id_token_hint: user?.id_token });
     await userManager.removeUser();
     localStorage.clear();
     navigate("/");
@@ -23,6 +24,13 @@ const LogoutButton = (props) => {
       onClickHandler={onLogoutButtonClick}
     />
   );
+};
+
+LogoutButton.propTypes = {
+  className: PropTypes.string,
+  user: PropTypes.shape({
+    id_token: PropTypes.string.isRequired,
+  }).isRequired,
 };
 
 export default LogoutButton;
