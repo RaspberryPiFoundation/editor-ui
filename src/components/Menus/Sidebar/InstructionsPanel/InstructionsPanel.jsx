2b72967f81976913c0e5e5377770ba2ff3dcbb6d
Lois Wells <lois.wells@raspberrypi.org>
2023-11-14 15:06:34 +0000

    initial instructions panel

---
+++ src/components/Menus/Sidebar/InstructionsPanel/InstructionsPanel.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import SidebarPanel from "../SidebarPanel";
+import { useTranslation } from "react-i18next";
+import { useSelector } from "react-redux";
+
+const InstructionsPanel = () => {
+  const steps = useSelector((state) => state.instructions.project.steps);
+  const currentStepPosition = useSelector(
+    (state) => state.instructions.currentStepPosition,
+  );
+  const { t } = useTranslation();
+  return (
+    <SidebarPanel heading={t("instructionsPanel.heading")}>
+      {steps[currentStepPosition].content}
+    </SidebarPanel>
+  );
+};
+
+export default InstructionsPanel;
