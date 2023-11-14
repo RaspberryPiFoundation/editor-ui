dcf745f863cc399d124344bbf944fd4af6770202
Scott <scott.adams@raspberrypi.org>
2023-11-14 09:14:06 +0000

    fix(storybook/main.js): add staticDirs path for static build of storybook in public directry

--- storybook/.storybook/main.js
+++ storybook/.storybook/main.js
@@ -9,6 +9,7 @@ module.exports = {
     "storybook-addon-root-attribute/register",
     "storybook-addon-react-router-v6",
   ],
+  staticDirs: ['../../public'],
   framework: "@storybook/react",
   features: {
     previewMdx2: true,
