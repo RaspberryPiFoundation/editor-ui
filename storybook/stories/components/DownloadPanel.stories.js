2dbfc9e2390196653a28404b09ea8f130d3dd894
Pete Simonovic <69108995+PetarSimonovic@users.noreply.github.com>
2023-11-16 09:24:20 +0000

    Reusable MockStore for Storybook

--- storybook/stories/components/DownloadPanel.stories.js
+++ storybook/stories/components/DownloadPanel.stories.js
@@ -1,63 +1,17 @@
 import React from "react";
 import DownloadPanel from "components/Menus/Sidebar/DownloadPanel/DownloadPanel";
-import { Provider } from "react-redux";
-import { configureStore, createSlice } from "@reduxjs/toolkit";
-import EditorSlice from "redux/EditorSlice";
+import MockStore from "../../store/MockStore";
 
 export default {
   title: "DownloadPanel",
   component: DownloadPanel,
 };
 
-const createMockStore = function (components, openFiles) {
-  return configureStore({
-    reducer: {
-      editor: createSlice({
-        name: "editor",
-        initialState: {
-          project: {
-            components,
-          },
-          isEmbedded: false,
-          openFiles,
-        },
-        reducer: EditorSlice,
-      }).reducer,
-    },
-  });
-};
-
-const store = createMockStore(
-  [
-    {
-      name: "a",
-      extension: "py",
-    },
-    {
-      name: "b",
-      extension: "html",
-    },
-    {
-      name: "c",
-      extension: "css",
-    },
-    {
-      name: "d",
-      extension: "csv",
-    },
-  ],
-  [[]],
-);
-
-const Mockstore = ({ children }) => (
-  <Provider store={store}>{children}</Provider>
-);
-
 const DefaultTemplate = () => {
   return (
-    <Mockstore>
+    <MockStore>
       <DownloadPanel />
-    </Mockstore>
+    </MockStore>
   );
 };
 
--- storybook/stories/components/FilePanel.stories.js
+++ storybook/stories/components/FilePanel.stories.js
@@ -1,64 +1,19 @@
 import React from "react";
 import FilePanel from "components/Menus/Sidebar/FilePanel/FilePanel";
-import EditorSlice from "redux/EditorSlice";
-import { configureStore, createSlice } from "@reduxjs/toolkit";
-import { Provider } from "react-redux";
+import MockStore from "../../store/MockStore";
 
 export default {
   title: "FilePanel",
   component: FilePanel,
 };
 
-const createMockStore = function (components, openFiles) {
-  return configureStore({
-    reducer: {
-      editor: createSlice({
-        name: "editor",
-        initialState: {
-          project: {
-            components,
-          },
-          isEmbedded: false,
-          openFiles,
-        },
-        reducer: EditorSlice,
-      }).reducer,
-    },
-  });
-};
-const store = createMockStore(
-  [
-    {
-      name: "a",
-      extension: "py",
-    },
-    {
-      name: "b",
-      extension: "html",
-    },
-    {
-      name: "c",
-      extension: "css",
-    },
-    {
-      name: "d",
-      extension: "csv",
-    },
-  ],
-  [[]],
-);
-
 const DefaultTemplate = () => {
   return (
-    <Mockstore>
+    <MockStore>
       <FilePanel />
-    </Mockstore>
+    </MockStore>
   );
 };
-
-const Mockstore = ({ children }) => (
-  <Provider store={store}>{children}</Provider>
-);
 
 export const Default = DefaultTemplate.bind();
 Default.args = {};
