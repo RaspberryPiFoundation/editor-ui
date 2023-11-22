481e5355473781ff01f04c1c6f931cd006497738
Scott <scott.adams@raspberrypi.org>
2023-11-22 15:29:29 +0000

    build(tidyup): edit some CI, remove build.sh

--- build.sh
+++
@@ -1,8 +0,0 @@
-# !/bin/bash
-# https://developers.cloudflare.com/pages/how-to/build-commands-branches/
-
-if [ "$CF_PAGES_BRANCH" != "production" ]; then
-  yarn run build
-else
-  yarn run build
-fi
