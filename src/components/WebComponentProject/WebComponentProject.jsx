4664f5e7ef3d79229b46cdacb277b683e2dc4358
Pete Simonovic <69108995+PetarSimonovic@users.noreply.github.com>
2023-12-08 08:42:45 +0000

    console logs removed

--- src/components/WebComponentProject/WebComponentProject.jsx
+++ src/components/WebComponentProject/WebComponentProject.jsx
@@ -24,10 +24,6 @@ const WebComponentProject = ({
   sidebarOptions = [],
   hostStyles = [],
 }) => {
-  console.log("HOST STYLES");
-  console.log(hostStyles);
-  console.log("Internal styles");
-  console.log(internalStyles);
   const project = useSelector((state) => state.editor.project);
   const codeRunTriggered = useSelector(
     (state) => state.editor.codeRunTriggered,
