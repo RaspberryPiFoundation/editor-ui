a6b1472926b129c14253078bbefa027d367235cd
Lois Wells <lois.wells@raspberrypi.org>
2024-01-10 11:25:37 +0000

    refactor using copilot into the correct style

--- src/components/Menus/Sidebar/InstructionsPanel/ProgressBar/ProgressBar2.test.js
+++ src/components/Menus/Sidebar/InstructionsPanel/ProgressBar/ProgressBar2.test.js
@@ -1,11 +1,10 @@
 import React from "react";
-import { render, fireEvent } from "@testing-library/react";
+import { render, fireEvent, screen } from "@testing-library/react";
 import { Provider } from "react-redux";
 import configureMockStore from "redux-mock-store";
 import ProgressBar from "./ProgressBar";
 import { setCurrentStepPosition } from "../../../../../redux/InstructionsSlice";
 
-// Create a mock store
 const mockStore = configureMockStore();
 let store;
 
@@ -18,51 +17,35 @@ beforeEach(() => {
       currentStepPosition: 1,
     },
   });
+
+  render(
+    <Provider store={store}>
+      <ProgressBar />
+    </Provider>,
+  );
 });
 
 describe("ProgressBar", () => {
-  it("renders without crashing", () => {
-    render(
-      <Provider store={store}>
-        <ProgressBar />
-      </Provider>,
-    );
+  test("renders without crashing", () => {
+    // Test will pass if render in beforeEach doesn't throw an error
   });
 
-  it("displays the correct progress", () => {
-    const { getByRole } = render(
-      <Provider store={store}>
-        <ProgressBar />
-      </Provider>,
-    );
-
-    const progress = getByRole("progressbar");
+  test("displays the correct progress", () => {
+    const progress = screen.getByRole("progressbar");
     expect(progress).toHaveAttribute("max", "2");
     expect(progress).toHaveAttribute("value", "1");
   });
 
-  it("dispatches setCurrentStepPosition with correct value when next button is clicked", () => {
-    const { getByTitle } = render(
-      <Provider store={store}>
-        <ProgressBar />
-      </Provider>,
-    );
-
-    const nextButton = getByTitle("instructionsPanel.nextStep");
+  test("dispatches setCurrentStepPosition with correct value when next button is clicked", () => {
+    const nextButton = screen.getByTitle("instructionsPanel.nextStep");
     fireEvent.click(nextButton);
 
     const actions = store.getActions();
     expect(actions[0]).toEqual(setCurrentStepPosition(2));
   });
 
-  it("dispatches setCurrentStepPosition with correct value when previous button is clicked", () => {
-    const { getByTitle } = render(
-      <Provider store={store}>
-        <ProgressBar />
-      </Provider>,
-    );
-
-    const previousButton = getByTitle("instructionsPanel.previousStep");
+  test("dispatches setCurrentStepPosition with correct value when previous button is clicked", () => {
+    const previousButton = screen.getByTitle("instructionsPanel.previousStep");
     fireEvent.click(previousButton);
 
     const actions = store.getActions();
