3851197914ee42ce2f6a1e3e489f37ef305588a2
Magdalena Jadach <magdalena@raspberrypi.org>
2024-01-15 12:46:01 +0000

    Linter

--- src/utils/externalLinkHelper.js
+++ src/utils/externalLinkHelper.js
@@ -11,32 +11,33 @@ const domain = `https://rpf.io/`;
 const rpfDomain = new RegExp(`^${domain}`);
 const allowedInternalLinks = [new RegExp(`^#[a-zA-Z0-9]+`)];
 const allowedExternalLinks = [rpfDomain];
+
 const useExternalLinkState = () => {
-  const [externalLink, setExternalLink] = useState();
+	const [externalLink, setExternalLink] = useState();
 
-  const handleAllowedExternalLink = (linkTo) => {
-    setExternalLink(linkTo);
-    dispatch(triggerCodeRun());
-  };
+	const handleAllowedExternalLink = (linkTo) => {
+		setExternalLink(linkTo);
+		dispatch(triggerCodeRun());
+	};
 
-  const handleRegularExternalLink = (linkTo, setPreviewFile) => {
-    setExternalLink(null);
-    setPreviewFile(`${linkTo}.html`);
-    dispatch(triggerCodeRun());
-  };
+	const handleRegularExternalLink = (linkTo, setPreviewFile) => {
+		setExternalLink(null);
+		setPreviewFile(`${linkTo}.html`);
+		dispatch(triggerCodeRun());
+	};
 
 	const handleExternalLinkError = () => {
 		dispatch(setError("externalLink"));
 		showModal();
 	};
 
-  return {
-    externalLink,
-    setExternalLink,
-    handleAllowedExternalLink,
-    handleRegularExternalLink,
+	return {
+		externalLink,
+		setExternalLink,
+		handleAllowedExternalLink,
+		handleRegularExternalLink,
 		handleExternalLinkError,
-  };
+	};
 };
 
 const matchingRegexes = (regexArray, testString) => {
