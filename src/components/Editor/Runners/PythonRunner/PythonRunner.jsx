e3a5e9a4b56daf9f7895caea1eae7be2a8b23f5c
Scott <scott.adams@raspberrypi.org>
2024-01-17 08:00:30 +0000

    fix(apiCallHandler): default to not sending errors to the backend

--- src/components/Editor/Runners/PythonRunner/PythonRunner.jsx
+++ src/components/Editor/Runners/PythonRunner/PythonRunner.jsx
@@ -14,6 +14,7 @@ import {
   triggerDraw,
 } from "../../../../redux/EditorSlice";
 import ErrorMessage from "../../ErrorMessage/ErrorMessage";
+import { createError } from "../../../../utils/apiCallHandler";
 import store from "../../../../app/store";
 import VisualOutputPane from "./VisualOutputPane";
 import OutputViewToggle from "./OutputViewToggle";
@@ -53,6 +54,10 @@ const externalLibraries = {
 
 const PythonRunner = () => {
   const projectCode = useSelector((state) => state.editor.project.components);
+  const projectIdentifier = useSelector(
+    (state) => state.editor.project.identifier,
+  );
+  const user = useSelector((state) => state.auth.user);
   const isSplitView = useSelector((state) => state.editor.isSplitView);
   const isEmbedded = useSelector((state) => state.editor.isEmbedded);
   const codeRunTriggered = useSelector(
@@ -280,7 +285,16 @@ const PythonRunner = () => {
       const lineNumber = err.traceback[0].lineno;
       const fileName = err.traceback[0].filename.replace(/^\.\//, "");
 
+      let userId;
+      if (user?.profile) {
+        userId = user.profile?.user;
+      }
+
       errorMessage = `${errorType}: ${errorDetails} on line ${lineNumber} of ${fileName}`;
+      createError(projectIdentifier, userId, {
+        errorType,
+        errorMessage,
+      });
     }
 
     dispatch(setError(errorMessage));
