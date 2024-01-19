0e220ad9052867c5a566a7dad1abdde2890275a9
Chris Patuzzo <chris@patuzzo.co.uk>
2024-01-19 12:32:14 +0000

    Print worker messages to the JavaScript console

--- src/components/Editor/Runners/PyodideRunner/PyodideRunner.jsx
+++ src/components/Editor/Runners/PyodideRunner/PyodideRunner.jsx
@@ -1,7 +1,16 @@
+import { useMemo } from "react";
+
 const PyodideRunner = () => {
-  const worker = new Worker("/webworkers/pyodideWorker.js", { type: "module" });
+  const worker = useMemo(loadPyodideWorker, []);
+
+  worker.onmessage = ({ data }) => {
+    console.log(data);
+  };
 
   return <p>Hello, World!</p>;
 };
+
+const loadPyodideWorker = () =>
+  new Worker("/webworkers/pyodideWorker.js", { type: "module" });
 
 export default PyodideRunner;
