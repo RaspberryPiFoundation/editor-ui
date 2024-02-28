f0ba6c331dfb2394cfd97dff37f11287d1fd4590
Lois Wells <lois.wells@raspberrypi.org>
2024-02-28 12:10:40 +0000

    tidying for linter

--- src/components/Editor/Output/Output.jsx
+++ src/components/Editor/Output/Output.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useSelector } from "react-redux";
 import ExternalFiles from "../../ExternalFiles/ExternalFiles";
 import RunnerFactory from "../Runners/RunnerFactory";
@@ -9,26 +9,12 @@ const Output = () => {
   const isEmbedded = useSelector((state) => state.editor.isEmbedded);
   const searchParams = new URLSearchParams(window.location.search);
   const isBrowserPreview = searchParams.get("browserPreview") === "true";
-  // const pythonInterpreter = useSelector(
-  //   (state) => state.runner.pythonInterpreter,
-  // );
-
-  // const [usePyodide, setUsePyodide] = useState(true);
-
-  // useEffect(() => {
-  //   if (pythonInterpreter === "skulpt") {
-  //     setUsePyodide(false);
-  //   }
-  // }, [pythonInterpreter]);
 
   return (
     <>
       <ExternalFiles />
       <div className="proj-runner-container">
-        <RunnerFactory
-          projectType={project.project_type}
-          // usePyodide={usePyodide}
-        />
+        <RunnerFactory projectType={project.project_type} />
         {isEmbedded && !isBrowserPreview ? <RunBar embedded /> : null}
       </div>
     </>
--- src/components/Editor/Runners/RunnerFactory.jsx
+++ src/components/Editor/Runners/RunnerFactory.jsx
@@ -1,68 +1,11 @@
-import React, { useEffect, useState } from "react";
-import PyodideRunner from "./PyodideRunner/PyodideRunner";
+import React from "react";
 import PythonRunner from "./PythonRunner/PythonRunner";
 import HtmlRunner from "./HtmlRunner/HtmlRunner";
-import { useSelector } from "react-redux";
-
-const SKULPT_ONLY_MODULES = ["p5", "py5", "sense_hat"];
 
 const RunnerFactory = ({ projectType }) => {
   const Runner = () => {
-    // const project = useSelector((state) => state.editor.project);
-    // const codeRunTriggered = useSelector(
-    //   (state) => state.editor.codeRunTriggered,
-    // );
-    // const [usePyodide, setUsePyodide] = useState(false);
-
-    // const getImports = (code) => {
-    //   const importRegex =
-    //     /^(from\s+([a-zA-Z0-9_\.]+)(\s+import\s+([a-zA-Z0-9_\.]+))?)|^(import\s+([a-zA-Z0-9_\.]+))/gm;
-    //   const matches = code.match(importRegex);
-    //   const imports = matches
-    //     ? matches.map(
-    //         (match) =>
-    //           match
-    //             .split(/from|import/)
-    //             .filter(Boolean)
-    //             .map((s) => s.trim())[0],
-    //       )
-    //     : [];
-    //   return imports;
-    // };
-
-    // useEffect(() => {
-    //   console.log("scanning imports");
-    //   project.components.forEach((component) => {
-    //     if (
-    //       projectType === "python" &&
-    //       component.extension === "py" &&
-    //       !codeRunTriggered
-    //     ) {
-    //       try {
-    //         const imports = getImports(component.content);
-    //         console.log(imports);
-    //         // const importNames = imports.map((importArr) => importArr[1]);
-    //         const hasSkulptOnlyModules = imports.some((name) =>
-    //           SKULPT_ONLY_MODULES.includes(name),
-    //         );
-    //         if (hasSkulptOnlyModules) {
-    //           console.log("using skulpt");
-    //           setUsePyodide(false);
-    //         } else {
-    //           console.log("using pyodide");
-    //           setUsePyodide(true);
-    //         }
-    //       } catch (error) {
-    //         console.error("Error occurred while getting imports:", error);
-    //       }
-    //     }
-    //   });
-    // }, [project, codeRunTriggered]);
-
     if (projectType === "html") {
       return HtmlRunner;
-      // } else if (usePyodide) {
-      //   return PyodideRunner;
     } else {
       return PythonRunner;
     }
