e74bd5b22acc592742d81fa2625955c0b0b93ab8
Chris Lowis <chris.lowis@gmail.com>
2024-04-09 16:52:49 +0100

    Look inside the shadow DOM in the Cypress tests
    The "renders the web component" test was failing because we include the
    editor using the "editor-wc" web component which creates elements in a
    shadow DOM and these elements are not accessible via the Cypress' `get`
    command. We need to use Cypress' `shadow` command [1] to look inside the
    shadow DOM. The test should now pass.
    
    [1] https://docs.cypress.io/api/commands/shadow
    
    Co-authored-by: Chris Roos <chris.roos@gofreerange.com>
    Co-authored-by: Chris Lowis <chris.lowis@gofreerange.com>

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,5 +2,5 @@ const baseUrl = "http://localhost:3012/en/projects/blank-python-starter";
 
 it("renders the web component", () => {
   cy.visit(baseUrl);
-  cy.get(".proj-container button").should("contain", "Run");
+  cy.get(".proj-container editor-wc").shadow().contains("Run").should("be.visible")
 });
