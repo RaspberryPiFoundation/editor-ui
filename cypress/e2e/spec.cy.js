cb7bdcca7a7337f963821e12ee5e123c889abbb8
James Mead <james.mead+rpf@gofreerange.com>
2024-04-10 12:22:43 +0100

    Remove all but one of the Cypress tests
    All of these tests were testing the behaviour of the `editor-ui` web
    component rather than that of the `editor-standalone` app. We plan to
    retain the tests in the `editor-ui` repo. We've just kept a single test
    in `editor-standalone` which checks that the editor web component has
    loaded/rendered OK. Note that the latter isn't working yet, but we'll
    fix that in a subsequent commit.
    
    Co-authored-by: Chris Roos <chris.roos@gofreerange.com>
    Co-authored-by: Chris Lowis <chris.lowis@gofreerange.com>

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -4,34 +4,3 @@ it("renders the web component", () => {
   cy.visit(baseUrl);
   cy.get(".proj-container button").should("contain", "Run");
 });
-
-it("Interrupts py5 draws when stop button clicked", () => {
-  cy.visit(baseUrl);
-  cy.get("div[class=cm-content]").invoke(
-    "text",
-    "import py5\ndef setup():\n\tsize(400, 400)\ndef draw():\n\tbackground(255)\nrun()",
-  );
-  cy.get(".btn--run").click();
-
-  cy.get(".btn--stop").click();
-  cy.get(".error-message__content").should("contain", "Execution interrupted");
-});
-
-it("Py5 magic comment imports py5", () => {
-  cy.visit(baseUrl);
-  cy.get("div[class=cm-content]").invoke("text", "# Py5: imported mode");
-  cy.get(".btn--run").click();
-
-  cy.get(".p5Canvas").should("be.visible");
-});
-
-it("Py5 imported mode runs sketch without explicit run call", () => {
-  cy.visit(baseUrl);
-  cy.get("div[class=cm-content]").invoke(
-    "text",
-    '# Py5: imported mode\ndef setup():\n\tsize(400,400)\n\ndef draw():\n\tprint("hello world")\nrun_sketch()',
-  );
-  cy.get(".btn--run").click();
-
-  cy.get(".pythonrunner-console-output-line").should("contain", "hello world");
-});
