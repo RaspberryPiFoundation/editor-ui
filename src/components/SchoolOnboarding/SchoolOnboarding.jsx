690033116fb0d7b769a84162da6ead65003cde3e
Lois Wells <lois.wells@raspberrypi.org>
2024-04-26 14:38:17 +0100

    redirecting to login before creating school

--- src/components/SchoolOnboarding/SchoolOnboarding.jsx
+++ src/components/SchoolOnboarding/SchoolOnboarding.jsx
@@ -12,13 +12,14 @@ import { login } from "../../utils/login";
 import { useLocation } from "react-router-dom";
 
 const SchoolOnboarding = () => {
+  const isLoadingUser = useSelector((state) => state.auth.isLoadingUser);
+  const user = useSelector((state) => state.auth.user);
   const accessToken = useSelector((state) => state.auth.user?.access_token);
   const { t } = useTranslation();
   const location = useLocation();
 
   useEffect(() => {
-    if (!accessToken) {
-      console.log("trying to get them to log in");
+    if (!isLoadingUser && !accessToken) {
       login({ location });
     }
     async function fetchData() {
@@ -33,66 +34,71 @@ const SchoolOnboarding = () => {
   });
 
   return (
-    <div className="school-onboarding-wrapper" data-testid="school-onboarding">
-      <div className="school-onboarding__text">
-        <h2 className="school-onboarding__title">
-          {t("schoolOnboarding.title_1")}
-        </h2>
-        <img src={LineIcon} alt="" />
-        <h2 className="school-onboarding__subtitle">
-          {t("schoolOnboarding.title_2")}
-        </h2>
+    user && (
+      <div
+        className="school-onboarding-wrapper"
+        data-testid="school-onboarding"
+      >
+        <div className="school-onboarding__text">
+          <h2 className="school-onboarding__title">
+            {t("schoolOnboarding.title_1")}
+          </h2>
+          <img src={LineIcon} alt="" />
+          <h2 className="school-onboarding__subtitle">
+            {t("schoolOnboarding.title_2")}
+          </h2>
+        </div>
+        <MultiStepForm />
+        <TextList
+          title={t("schoolCreated.title")}
+          titleIcon={TexListTickIcon}
+          text={t("schoolCreated.text")}
+          next={t("schoolCreated.next")}
+          imageSrc={TextListImageExample}
+          imageAlt=""
+          contact={t("schoolCreated.contact")}
+          listItems={{
+            item1:
+              "Thank you for providing the all the information needed to set up your school account.",
+            item2:
+              "We will verify your school. This may take up to 5 working days. You'll receive a confirmation email once it's been verified.",
+            item3:
+              "Once your school has been verified, you will be able to log in to the Code Editor with your Raspberry Pi Foundation account and access your school dashboard.",
+          }}
+          exploreProjects={{
+            text: t("schoolCreated.exploreProjects.text"),
+            url: t("schoolCreated.exploreProjects.url"),
+            plausible: t("schoolCreated.exploreProjects.plausible"),
+          }}
+          editorHome={{
+            text: t("schoolCreated.editorHome.text"),
+            url: t("schoolCreated.editorHome.url"),
+            plausible: t("schoolCreated.editorHome.plausible"),
+          }}
+        />
+        <TextList
+          title={t("schoolBeingVerified.title")}
+          text={t("schoolBeingVerified.text")}
+          next={t("schoolBeingVerified.next")}
+          contact={t("schoolBeingVerified.contact")}
+          listItems={{
+            item1: t("schoolBeingVerified.listItems.item1"),
+            item2: t("schoolBeingVerified.listItems.item2"),
+            item3: t("schoolBeingVerified.listItems.item3"),
+          }}
+          exploreProjects={{
+            text: t("schoolBeingVerified.exploreProjects.text"),
+            url: t("schoolBeingVerified.exploreProjects.url"),
+            plausible: t("schoolBeingVerified.exploreProjects.plausible"),
+          }}
+          editorHome={{
+            text: t("schoolBeingVerified.editorHome.text"),
+            url: t("schoolBeingVerified.editorHome.url"),
+            plausible: t("schoolBeingVerified.editorHome.plausible"),
+          }}
+        />
       </div>
-      <MultiStepForm />
-      <TextList
-        title={t("schoolCreated.title")}
-        titleIcon={TexListTickIcon}
-        text={t("schoolCreated.text")}
-        next={t("schoolCreated.next")}
-        imageSrc={TextListImageExample}
-        imageAlt=""
-        contact={t("schoolCreated.contact")}
-        listItems={{
-          item1:
-            "Thank you for providing the all the information needed to set up your school account.",
-          item2:
-            "We will verify your school. This may take up to 5 working days. You'll receive a confirmation email once it's been verified.",
-          item3:
-            "Once your school has been verified, you will be able to log in to the Code Editor with your Raspberry Pi Foundation account and access your school dashboard.",
-        }}
-        exploreProjects={{
-          text: t("schoolCreated.exploreProjects.text"),
-          url: t("schoolCreated.exploreProjects.url"),
-          plausible: t("schoolCreated.exploreProjects.plausible"),
-        }}
-        editorHome={{
-          text: t("schoolCreated.editorHome.text"),
-          url: t("schoolCreated.editorHome.url"),
-          plausible: t("schoolCreated.editorHome.plausible"),
-        }}
-      />
-      <TextList
-        title={t("schoolBeingVerified.title")}
-        text={t("schoolBeingVerified.text")}
-        next={t("schoolBeingVerified.next")}
-        contact={t("schoolBeingVerified.contact")}
-        listItems={{
-          item1: t("schoolBeingVerified.listItems.item1"),
-          item2: t("schoolBeingVerified.listItems.item2"),
-          item3: t("schoolBeingVerified.listItems.item3"),
-        }}
-        exploreProjects={{
-          text: t("schoolBeingVerified.exploreProjects.text"),
-          url: t("schoolBeingVerified.exploreProjects.url"),
-          plausible: t("schoolBeingVerified.exploreProjects.plausible"),
-        }}
-        editorHome={{
-          text: t("schoolBeingVerified.editorHome.text"),
-          url: t("schoolBeingVerified.editorHome.url"),
-          plausible: t("schoolBeingVerified.editorHome.plausible"),
-        }}
-      />
-    </div>
+    )
   );
 };
 
