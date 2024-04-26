88a06b30f9cf19f285a8f2a321742e7c8b492840
Conor <conor.riches@raspberrypi.org>
2024-04-26 01:02:20 +0100

    update util test

--- src/utils/fieldValidation.js
+++ src/utils/fieldValidation.js
@@ -4,10 +4,14 @@ export const fieldError = ({ errorFields, fieldName, errorMessage }) => {
 
 export const existsValidation = ({ stepData, fieldName }) => {
   if (!stepData[fieldName]) return fieldName;
+
+  return false;
 };
 
 export const urlValidation = ({ stepData, fieldName }) => {
   if (!stepData[fieldName]) return fieldName;
   if (!stepData[fieldName].match(/[a-zA-Z0-9_\-]+\.[a-zA-Z0-9_\-]+/))
     return fieldName;
+
+  return false;
 };
