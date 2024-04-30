8a7c7476c6955c6c71dffe79edb461aaed0a71b6
Lois Wells <lois.wells@raspberrypi.org>
2024-04-30 17:35:17 +0100

    actually fixing linting

--- src/components/SchoolOnboarding/SchoolOnboarding.jsx
+++ src/components/SchoolOnboarding/SchoolOnboarding.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from "react";
-import { useTranslation } from "react-i18next";
 import "../../assets/stylesheets/SchoolOnboarding.scss";
 import LogoLM from "../LogoLM/LogoLM";
 import MultiStepForm from "./MultistepForm";
