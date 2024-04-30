aee117a689604379c5a78db5a66269bc4700c879
Magdalena Jadach <magdalena.jadach@MagdalenaJadach-MBPP-M2.local>
2024-04-30 11:08:13 +0100

    Add missing copy

--- src/components/EducationLandingPage/EducationLandingPage.jsx
+++ src/components/EducationLandingPage/EducationLandingPage.jsx
@@ -35,7 +35,7 @@ const EducationLandingPage = () => {
 
   return (
     <div className="education-landing-page-wrapper">
-      <h1>header</h1>
+      <h1>header placeholder</h1>
       <TextImage
         title="An IDE designed for learners"
         text="Tailored specifically to young people's needs, our integrated development environment (IDE) helps make learning text-based programming simple and accessible for children aged 9 and up. It’s safe, age-appropriate, and suitable for use in the classroom."
@@ -43,7 +43,7 @@ const EducationLandingPage = () => {
         imageSrc={editorScreenshot}
       />
       <TextImage
-        title="Create engaging coding lessons"
+        title="All features, totally free"
         text="Create custom coding projects for your students to work on. Help them code their own games and art using Python, or design websites in HTML/CSS/JavaScript.
         Your students can get creative with code with our wide choice of Python libraries!"
         imageAlt="Editor output"
@@ -51,12 +51,35 @@ const EducationLandingPage = () => {
         imagePosition="right"
       />
       <TextImage
+        title="Create engaging coding lessons"
+        text="Create custom coding projects for your students to work on. Help them code their own games and art using Python, or design websites in HTML/CSS/JavaScript.
+
+        Your students can get creative with code with our wide choice of Python libraries!
+        "
+        imageAlt="Editor screenshot"
+        imageSrc={editorScreenshot}
+      />
+      <TextImage
         title="Give students personalised feedback"
         text="Easily check your students’ progress, view their coding projects, and share individual, instant feedback with each student to keep them on track."
         imageAlt="Editor output"
         imageSrc={editorOutput}
       />
-
+      <TextImage
+        title="Simple and easy class managemen"
+        text="Like the Editor itself, we’ve kept our educator interface clean, simple, and easy to use following feedback from teachers. Create and manage student accounts easily. You can organise your students into classes and help them reset their passwords quickly."
+        imageAlt="Editor output"
+        imageSrc={editorOutput}
+      />
+      <TextImage
+        title="Safe and private by design"
+        text="Accounts for education are designed to be safe for students of all ages. 
+        We take safeguarding seriously, with one-way feedback to students, visibility of their work at all times, verified school accounts, and an audit history of communication with students. 
+        
+        In line with best practice codes protecting children online, we minimise our data capture so that we have just enough to keep students safe."
+        imageAlt="Editor output"
+        imageSrc={editorOutput}
+      />
       <TextImage
         title="What do people say about our Editor?"
         text="“We have used it and love it, the fact that it is both for HTML / CSS and then Python is great as the students have a one-stop shop for IDEs.” – Lee Willis, Head of ICT and Computing, Newcastle High School for Girls
@@ -66,7 +89,6 @@ const EducationLandingPage = () => {
         imageSrc={classroom}
       />
       <div className="education-landing-page__get-started">
-        {" "}
         <h2 className="school-onboarding__subtitle">
           {t("educationLandingPage.title")}
         </h2>
