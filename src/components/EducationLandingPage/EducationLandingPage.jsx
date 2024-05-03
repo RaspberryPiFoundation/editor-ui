010ec1adf1c8fcb5f55d4e3772d5da239104ac6f
Magdalena Jadach <magdalena.jadach@MagdalenaJadach-MBPP-M2.local>
2024-05-03 12:56:15 +0100

    Address review changes

--- src/components/EducationLandingPage/EducationLandingPage.jsx
+++ src/components/EducationLandingPage/EducationLandingPage.jsx
@@ -26,57 +26,48 @@ const EducationLandingPage = () => {
     >
       <Hero />
       <TextImage
-        title="An IDE designed for learners"
-        text="Tailored specifically to young people's needs, our integrated development environment (IDE) helps make learning text-based programming simple and accessible for children aged 9 and up. It’s safe, age-appropriate, and suitable for use in the classroom."
-        imageAlt="Editor screenshot"
+        title={t("educationLandingPage.ide.title")}
+        text={t("educationLandingPage.ide.text")}
+        imageAlt={t("educationLandingPage.ide.imageAlt")}
         imageSrc={editorScreenshot}
       />
       <TextImage
-        title="All features, totally free"
-        text="Create custom coding projects for your students to work on. Help them code their own games and art using Python, or design websites in HTML/CSS/JavaScript.
-        Your students can get creative with code with our wide choice of Python libraries!"
-        imageAlt="Editor output"
+        title={t("educationLandingPage.free.title")}
+        text={t("educationLandingPage.free.text")}
+        imageAlt={t("educationLandingPage.free.imageAlt")}
         imageSrc={editorOutput}
         imagePosition="right"
       />
       <TextImage
-        title="Create engaging coding lessons"
-        text="Create custom coding projects for your students to work on. Help them code their own games and art using Python, or design websites in HTML/CSS/JavaScript.
-
-        Your students can get creative with code with our wide choice of Python libraries!
-        "
-        imageAlt="Editor screenshot"
+        title={t("educationLandingPage.engage.title")}
+        text={t("educationLandingPage.engage.text")}
+        imageAlt={t("educationLandingPage.engage.imageAlt")}
         imageSrc={placeholder}
       />
       <TextImage
-        title="Give students personalised feedback"
-        text="Easily check your students’ progress, view their coding projects, and share individual, instant feedback with each student to keep them on track."
-        imageAlt="Editor output"
+        title={t("educationLandingPage.feedback.title")}
+        text={t("educationLandingPage.feedback.text")}
+        imageAlt={t("educationLandingPage.feedback.imageAlt")}
         imageSrc={placeholder}
         imagePosition="right"
       />
       <TextImage
-        title="Simple and easy class management"
-        text="Like the Editor itself, we’ve kept our educator interface clean, simple, and easy to use following feedback from teachers. Create and manage student accounts easily. You can organise your students into classes and help them reset their passwords quickly."
-        imageAlt="Editor output"
+        title={t("educationLandingPage.class.title")}
+        text={t("educationLandingPage.class.text")}
+        imageAlt={t("educationLandingPage.class.imageAlt")}
         imageSrc={placeholder}
       />
       <TextImage
-        title="Safe and private by design"
-        text="Accounts for education are designed to be safe for students of all ages. 
-        We take safeguarding seriously, with one-way feedback to students, visibility of their work at all times, verified school accounts, and an audit history of communication with students. 
-        
-        In line with best practice codes protecting children online, we minimise our data capture so that we have just enough to keep students safe."
-        imageAlt="Editor output"
+        title={t("educationLandingPage.safe.title")}
+        text={t("educationLandingPage.safe.text")}
+        imageAlt={t("educationLandingPage.safe.imageAlt")}
         imageSrc={editorOutput}
         imagePosition="right"
       />
       <TextImage
-        title="What do people say about our Editor?"
-        text="“We have used it and love it, the fact that it is both for HTML / CSS and then Python is great as the students have a one-stop shop for IDEs.” – Lee Willis, Head of ICT and Computing, Newcastle High School for Girls
-
-        “The class management and feedback features they're working on at the moment look really promising.” – Pete Dring, Head of Computing, Fulford School"
-        imageAlt="Classroom"
+        title={t("educationLandingPage.people.title")}
+        text={t("educationLandingPage.people.text")}
+        imageAlt={t("educationLandingPage.people.imageAlt")}
         imageSrc={classroom}
       />
       <div className="education-landing-page__get-started">
