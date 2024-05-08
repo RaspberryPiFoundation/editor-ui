f858b5bf36d065cf4211afd43858c4a552e47625
Lois Wells <lois.wells@raspberrypi.org>
2024-05-08 12:10:42 +0100

    put the redirection to your projects back in for now

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,5 +1,6 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
 import { useCookies } from "react-cookie";
 import { useTranslation } from "react-i18next";
 import "../../assets/stylesheets/LandingPage.scss";
@@ -11,6 +12,8 @@ import { ReactComponent as HtmlFileIcon } from "../../assets/icons/html_file.svg
 import { ReactComponent as PythonFileIcon } from "../../assets/icons/python_file.svg";
 
 const LandingPage = () => {
+  const navigate = useNavigate();
+  const user = useSelector((state) => state.auth.user);
   const { t, i18n } = useTranslation();
   const [cookies] = useCookies(["theme"]);
   const locale = i18n.language;
@@ -24,6 +27,12 @@ const LandingPage = () => {
       window.plausible(msg);
     }
   };
+
+  useEffect(() => {
+    if (user) {
+      navigate(`/${locale}/projects`);
+    }
+  }, [user, locale, navigate]);
 
   return (
     <div className="landing-page-wrapper">
