e776b01cee6579bae044c46faf6238d275315fd7
Max Elkins <hello@maxelkins.me>
2024-05-09 21:40:26 +0100

    Fixed spacing and style of slices
    - Moved slices into a main tag
    - Moved spacing to main tag rather than on slices
    - Updated custom properties to use Design System
    - Changed media queries to match designs.
    - Removed light/dark mode styles as this is now done via custom properties.
    - Fixed spacing within slices
    - Fixed alignment of content within slices

--- src/components/EducationLandingPage/EducationLandingPage.jsx
+++ src/components/EducationLandingPage/EducationLandingPage.jsx
@@ -25,57 +25,59 @@ const EducationLandingPage = () => {
       data-testid="education-landing-page"
     >
       <Hero />
-      <TextImage
-        title={t("educationLandingPage.ide.title")}
-        text={t("educationLandingPage.ide.text")}
-        imageAlt={t("educationLandingPage.ide.imageAlt")}
-        imageSrc={editorScreenshot}
-      />
-      <TextImage
-        title={t("educationLandingPage.free.title")}
-        text={t("educationLandingPage.free.text")}
-        imageAlt={t("educationLandingPage.free.imageAlt")}
-        imageSrc={editorOutput}
-        imagePosition="right"
-      />
-      <TextImage
-        title={t("educationLandingPage.engage.title")}
-        text={t("educationLandingPage.engage.text")}
-      />
-      <TextImage
-        title={t("educationLandingPage.feedback.title")}
-        text={t("educationLandingPage.feedback.text")}
-        imageAlt={t("educationLandingPage.feedback.imageAlt")}
-        imageSrc={placeholder}
-        imagePosition="right"
-      />
-      <TextImage
-        title={t("educationLandingPage.class.title")}
-        text={t("educationLandingPage.class.text")}
-      />
-      <TextImage
-        title={t("educationLandingPage.people.title")}
-        text={t("educationLandingPage.people.text")}
-        imageAlt={t("educationLandingPage.people.imageAlt")}
-        o
-        imageSrc={classroom}
-      />
-      <TextImage
-        title={t("educationLandingPage.safe.title")}
-        text={t("educationLandingPage.safe.text")}
-      />
-      <div className="education-landing-page__get-started">
-        <h2 className="school-onboarding__subtitle">
-          {t("educationLandingPage.title")}
-        </h2>
-        <DesignSystemButton
-          className="landing-page__button"
-          href={`/${locale}/`}
-          text={t("educationLandingPage.start")}
-          textAlways
-          onClick={onClickPlausible("Create your school accout")}
+      <main>
+        <TextImage
+          title={t("educationLandingPage.ide.title")}
+          text={t("educationLandingPage.ide.text")}
+          imageAlt={t("educationLandingPage.ide.imageAlt")}
+          imageSrc={editorScreenshot}
         />
-      </div>
+        <TextImage
+          title={t("educationLandingPage.free.title")}
+          text={t("educationLandingPage.free.text")}
+          imageAlt={t("educationLandingPage.free.imageAlt")}
+          imageSrc={editorOutput}
+          imagePosition="right"
+        />
+        <TextImage
+          title={t("educationLandingPage.engage.title")}
+          text={t("educationLandingPage.engage.text")}
+        />
+        <TextImage
+          title={t("educationLandingPage.feedback.title")}
+          text={t("educationLandingPage.feedback.text")}
+          imageAlt={t("educationLandingPage.feedback.imageAlt")}
+          imageSrc={placeholder}
+          imagePosition="right"
+        />
+        <TextImage
+          title={t("educationLandingPage.class.title")}
+          text={t("educationLandingPage.class.text")}
+        />
+        <TextImage
+          title={t("educationLandingPage.people.title")}
+          text={t("educationLandingPage.people.text")}
+          imageAlt={t("educationLandingPage.people.imageAlt")}
+          o
+          imageSrc={classroom}
+        />
+        <TextImage
+          title={t("educationLandingPage.safe.title")}
+          text={t("educationLandingPage.safe.text")}
+        />
+        <div className="education-landing-page__get-started">
+          <h2 className="school-onboarding__subtitle">
+            {t("educationLandingPage.title")}
+          </h2>
+          <DesignSystemButton
+            className="landing-page__button"
+            href={`/${locale}/`}
+            text={t("educationLandingPage.start")}
+            textAlways
+            onClick={onClickPlausible("Create your school accout")}
+          />
+        </div>
+      </main>
     </div>
   );
 };
