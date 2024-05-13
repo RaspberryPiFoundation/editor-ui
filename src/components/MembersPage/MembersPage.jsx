edd2d8689855a58827da7143f353fbcd5bceffea
Magdalena Jadach <magdalena.jadach@gmail.com>
2024-05-13 12:20:14 +0100

    Merge and removal

--- src/components/MembersPage/MembersPage.jsx
+++ src/components/MembersPage/MembersPage.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { useTranslation } from "react-i18next";
 import "../../assets/stylesheets/MembersPage.scss";
 import MembersPageHeader from "./MembersPageHeader/MambersPageHeader";
 
 const MembersPage = () => {
-  const { t } = useTranslation();
   const userRoles = useSelector(
     (state) => state.auth.user?.profile?.roles,
   )?.split(",");
