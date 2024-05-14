dd4458a2af32980eb35c2ea1a88c64af59b50c78
Max Elkins <hello@maxelkins.me>
2024-05-14 16:39:39 +0100

    fix: LogoLm styles
    - Updated to design system sizes
    - Updated media queries
    - Removed need for `line.svg`

--- src/components/LogoLM/LogoLM.jsx
+++ src/components/LogoLM/LogoLM.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
 import "../../assets/stylesheets/LogoLM.scss";
-import LineIcon from "../../assets/icons/line.svg";
 
 const LogoLM = () => {
   const { t } = useTranslation();
@@ -10,7 +9,6 @@ const LogoLM = () => {
     <div className="logo-lm-wrapper" data-testid="logo-lm">
       <div className="logo-lm__text">
         <h2 className="logo-lm__title">{t("logoLM.codeEditor")}</h2>
-        <img src={LineIcon} alt="" />
         <h2 className="logo-lm-__subtitle">{t("logoLM.forEducation")}</h2>
       </div>
     </div>
