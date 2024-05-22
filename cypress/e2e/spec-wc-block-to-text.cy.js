7a0bf739208e95f09ac89c278de28ae8cebd7154
James Mead <james.mead+rpf@gofreerange.com>
2024-05-22 13:45:53 +0100

    WIP: Add e2e spec for wc in Block-to-Text mode
    This adds a new e2e spec which loads the web component with its
    attributes set to mimic the usage by Block-to-Text alpha app [1,2].
    
    * The `assets_identifier` attribute is set to load the assets (but not
      the code) from the "encoded-art-starter" project which includes a
      `moon.png` image.
    * The `host_styles` attribute is set partly because it is used by the
      Block-to-Text app, but also because otherwise the output-only editor
      has zero height in the Electron browser used by default by Cypress.
    * The `code` attribute is set to load a simple P5 sketch which draws the
      word "PASS" in green and draws the `moon.png` image on the canvas.
    * The `output_only` attribute is set to `true` so that only the output
      pane of the editor is displayed; not the code editor, the run bar,
      etc.
    * The `output_panels` attribute is set to `["visual"]` so that only the
      visual output canvas is displayed; not the text output.
    * A "Run code" button has been added to `src/web-component.html` to
      mimic a similar button in the Block-to-Text app which has an `onclick`
      handler to call the `rerunCode` method on the web component.
    
    Note that the "embedded-viewer" CSS class that we're asserting against
    is added by the `WebComponentProject` component around the `Output`
    component; not by the `EmbeddedViewer` component which is currently only
    used via the `/:locale/embed/viewer/:identifier` route which is used in
    an iframe by e.g. the Projects app.
    
    By asserting the results contain '{"errorDetails":{}}' we can have some
    confidence that the P5 sketch code has run successfully.
    
    TODO:
    * Do we need to set the `embedded` attribute to `true` like the
      Block-to-Text app? It seems unnecessary.
    * The drawing of the `moon.png` image doesn't seem to work at all in
      Electron or Chrome and only patchily in Firefox (and even then only
      when caching is not disabled). Does this highlight an actual problem?
    * If `assets_identifier` is not set, the spec doesn't fail in Electron,
      although an uncaught exception is thrown when the image is fetched. Is
      this a sufficient test? Also does the uncaught exception highlight an
      actual problem - shouldn't it be caught?
    
    [1]: https://block-to-text-alpha.pages.dev/
    [2]: https://github.com/RaspberryPiFoundation/block-to-text-alpha/

---
+++ cypress/e2e/spec-wc-block-to-text.cy.js
@@ -0,0 +1,39 @@
+const code = `
+from p5 import *
+
+img = None
+
+def setup():
+    size(400, 200)
+    global img
+    img = load_image("moon.png")
+
+def draw():
+    fill("green")
+    text_size(100)
+    text("PASS", 125, 110)
+    image(img, 10, 25, 100 , 100)
+    no_loop()
+
+run()
+`;
+
+const params = new URLSearchParams();
+params.set("assets_identifier", "encoded-art-starter");
+params.set("host_styles", JSON.stringify("#wc { min-height: 200px }"));
+params.set("code", code);
+params.set("output_only", true);
+params.set("output_panels", JSON.stringify(["visual"]));
+
+const baseUrl = `http://localhost:3001?${params.toString()}`;
+
+it("runs p5 sketch including image loaded from project ", () => {
+  cy.visit(baseUrl);
+
+  cy.get("editor-wc").shadow().find(".embedded-viewer").should("exist");
+  cy.get("editor-wc").shadow().find(".proj").should("not.exist");
+
+  cy.get("button").contains("Run code").click();
+
+  cy.get("#results").should("contain", '{"errorDetails":{}}');
+});
