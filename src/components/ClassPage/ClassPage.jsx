2c859eadb1e758bcec78c45a8db3d09a8de00703
Magdalena Jadach <magdalena.jadach@gmail.com>
2024-05-24 10:51:09 +0100

    Test and final tweaks

--- src/components/ClassPage/ClassPage.jsx
+++ src/components/ClassPage/ClassPage.jsx
@@ -10,7 +10,7 @@ import "../../assets/stylesheets/ClassPage.scss";
 const ClassPage = () => {
   const { t } = useTranslation();
   return (
-    <div className="class_wrapper">
+    <div data-testid="class-page" className="class_wrapper">
       <div className="class__copy">
         <h1 className="class__title">{t("classPage.title")}</h1>
         <p className="class__text">{t("classPage.text")}</p>
