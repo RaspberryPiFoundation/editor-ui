55421dca307f22b179629b22e488e9f3455b58d2
Pete Simonovic <69108995+PetarSimonovic@users.noreply.github.com>
2024-06-12 11:27:59 +0100

    Restore missing outputPanels prop

--- src/components/Editor/Runners/RunnerFactory.jsx
+++ src/components/Editor/Runners/RunnerFactory.jsx
@@ -4,7 +4,12 @@ import PythonRunner from "./PythonRunner/PythonRunner";
 import MicroPythonRunner from "./MicroPythonRunner/MicroPythonRunner";
 import HtmlRunner from "./HtmlRunner/HtmlRunner";
 
-const RunnerFactory = ({ isPico, projectType, usePyodide }) => {
+const RunnerFactory = ({
+  isPico,
+  projectType,
+  usePyodide,
+  outputPanels = ["text", "visual"],
+}) => {
   const Runner = () => {
     if (projectType === "html") {
       return HtmlRunner;
