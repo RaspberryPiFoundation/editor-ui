16b470700e4520f3e73848bcb0f981782306ad11
Scott <scott.adams@raspberrypi.org>
2024-09-03 17:03:36 +0100

    fix p5/py5 Sk scope related errors by moving to the window

--- src/components/Editor/Runners/PythonRunner/VisualOutputPane.jsx
+++ src/components/Editor/Runners/PythonRunner/VisualOutputPane.jsx
@@ -30,13 +30,16 @@ const VisualOutputPane = () => {
       pygalOutput.current.innerHTML = "";
       p5Output.current.innerHTML = "";
 
-      Sk.py5 = {};
-      Sk.py5.sketch = "p5Sketch";
-      Sk.py5.assets = projectImages;
+      if (!window.py5) {
+        window.py5 = {};
+      }
+      window.py5.sketch = "p5Sketch";
 
-      Sk.p5 = {};
-      Sk.p5.sketch = "p5Sketch";
-      Sk.p5.assets = projectImages;
+      if (!window.p5) {
+        window.p5 = {};
+      }
+      window.p5.sketch = "p5Sketch";
+      window.assets = projectImages;
 
       (Sk.pygal || (Sk.pygal = {})).outputCanvas = pygalOutput.current;
 
@@ -56,10 +59,14 @@ const VisualOutputPane = () => {
       p5Output.current &&
       p5Output.current.innerHTML !== ""
     ) {
-      if (Sk.py5.stop) {
+      if (Sk.py5?.stop) {
         Sk.py5.stop();
-      } else {
+      } else if (Sk.p5?.stop) {
         Sk.p5.stop();
+      } else if (window.p5?.stop) {
+        window.p5.stop();
+      } else if (window.py5?.stop) {
+        window.py5.stop();
       }
 
       if (error === "") {
