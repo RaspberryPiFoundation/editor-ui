3a50dc317815bfc375ec6ab2bf113eecec3e9394
Lois Wells <lois.wells@raspberrypi.org>
2024-10-03 11:48:45 +0100

    trying to fix cypress tests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -5,14 +5,14 @@ it("renders the web component", () => {
   cy.get(".proj-container button").should("contain", "Run");
 });
 
-it("Interrupts py5 draws when stop button clicked", () => {
+it.only("Interrupts py5 draws when stop button clicked", () => {
   cy.visit(baseUrl);
   cy.get("div[class=cm-content]").invoke(
     "text",
     "import py5\ndef setup():\n\tpy5.size(400, 400)\ndef draw():\n\tpy5.background(255)\npy5.run_sketch()",
   );
   cy.get(".btn--run").click();
-
+  cy.wait(1000);
   cy.get(".btn--stop").click();
   cy.get(".error-message__content").should("contain", "Execution interrupted");
 });
@@ -29,7 +29,7 @@ it("Py5 imported mode runs sketch without explicit run call", () => {
   cy.visit(baseUrl);
   cy.get("div[class=cm-content]").invoke(
     "text",
-    '# Py5: imported mode\ndef setup():\n\tsize(400,400)\n\ndef draw():\n\tprint("hello world")\nrun_sketch()',
+    '# Py5: imported mode\ndef setup():\n\tsize(400,400)\n\ndef draw():\n\tprint("hello world")',
   );
   cy.get(".btn--run").click();
 
