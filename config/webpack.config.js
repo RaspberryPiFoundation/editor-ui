54fe3996ab82bda79f93c5d22796dac06b1b75e8
Lois Wells <lois.wells@raspberrypi.org>
2024-10-04 12:53:07 +0100

    even more tidying

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -544,18 +544,6 @@ module.exports = function (webpackEnv) {
                 svgoConfig: SVGOConfig,
               },
             },
-            {
-              test: /\.worker\.js$/,
-              use: [
-                { loader: "worker-loader" },
-                {
-                  loader: "babel-loader",
-                  options: {
-                    presets: ["@babel/preset-env"],
-                  },
-                },
-              ],
-            },
             // "file" loader makes sure those assets get served by WebpackDevServer.
             // When you `import` an asset, you get its (virtual) filename.
             // In production, they would get copied to the `build` folder.
