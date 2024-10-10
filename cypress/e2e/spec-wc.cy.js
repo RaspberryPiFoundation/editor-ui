277b3e2f463e63dcb19dcd376fd5eee94813672c
Lois Wells <lois.wells@raspberrypi.org>
2024-10-10 16:56:06 +0100

    removing duplicate tests

--- cypress/e2e/spec-wc.cy.js
+++ cypress/e2e/spec-wc.cy.js
@@ -26,34 +26,6 @@ describe("default behaviour", () => {
       .should("contain", "Text output");
   });
 
-  it("runs the python code", () => {
-    cy.get("editor-wc")
-      .shadow()
-      .find("div[class=cm-content]")
-      .invoke("text", 'print("Hello world")');
-    cy.get("editor-wc").shadow().find(".btn--run").click();
-    cy.get("editor-wc")
-      .shadow()
-      .find(".pythonrunner-console-output-line")
-      .should("contain", "Hello world");
-  });
-
-  it("interrupts the code when the stop button is clicked", () => {
-    cy.get("editor-wc")
-      .shadow()
-      .find("div[class=cm-content]")
-      .invoke(
-        "text",
-        "from time import sleep\nfor i in range(100):\n\tprint(i)\n\tsleep(1)",
-      );
-    cy.get("editor-wc").shadow().find(".btn--run").click();
-    cy.get("editor-wc").shadow().find(".btn--stop").click();
-    cy.get("editor-wc")
-      .shadow()
-      .find(".error-message__content")
-      .should("contain", "Execution interrupted");
-  });
-
   it("does not render visual output tab on page load", () => {
     cy.get("editor-wc")
       .shadow()
