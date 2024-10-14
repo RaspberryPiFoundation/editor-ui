08a94f5b44591365baf498ef96708c4f15e57e5f
Lois Wells <lois.wells@raspberrypi.org>
2024-10-14 12:53:50 +0100

    more lighting updates

--- src/components/AstroPiModel/Lighting.jsx
+++ src/components/AstroPiModel/Lighting.jsx
@@ -5,7 +5,7 @@ const Lighting = () => {
     <>
       <ambientLight intensity={0.25 * Math.PI} />
       <pointLight
-        intensity={50}
+        intensity={265}
         angle={28}
         penumbra={2}
         position={[0.418, 16.199, 0.3]}
@@ -13,26 +13,26 @@ const Lighting = () => {
       />
       <pointLight
         position={[-16.116, 14.37, 8.208]}
-        intensity={125}
+        intensity={375}
         castShadow
       />
       <pointLight
         position={[-16.109, 18.021, -8.207]}
-        intensity={125}
+        intensity={375}
         castShadow
       />
       <pointLight
         position={[14.904, 12.198, -1.832]}
-        intensity={43}
+        intensity={125}
         castShadow
       />
-      <pointLight position={[-0.462, 8.89, 14.52]} intensity={108} castShadow />
+      <pointLight position={[-0.462, 8.89, 14.52]} intensity={325} castShadow />
       <pointLight
         position={[3.235, 11.486, -12.541]}
-        intensity={100}
+        intensity={150}
         castShadow
       />
-      <pointLight position={[0.0, -20.0, 0.0]} intensity={125} castShadow />
+      <pointLight position={[0.0, -20.0, 0.0]} intensity={375} castShadow />
     </>
   );
 };
