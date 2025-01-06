80eeb83d05ef482929035cf3563ffe1270b3bb2b
Lois Wells <lois.wells@raspberrypi.org>
2025-01-06 12:30:08 +0000

    testing pythonrunner passes output panels prop to skulptrunner

--- src/components/Editor/Runners/PythonRunner/PythonRunner.test.js
+++ src/components/Editor/Runners/PythonRunner/PythonRunner.test.js
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react";
+import { render, within } from "@testing-library/react";
 import { Provider } from "react-redux";
 import { act } from "react-dom/test-utils";
 import PythonRunner from "./PythonRunner";
@@ -56,110 +56,151 @@ const updateRunner = ({
     store.dispatch(setSenseHatAlwaysEnabled(senseHatAlwaysEnabled));
   });
 };
+describe("PythonRunner with default props", () => {
+  beforeEach(() => {
+    window.crossOriginIsolated = true;
+    render(
+      <Provider store={store}>
+        <PythonRunner />
+      </Provider>,
+    );
+    updateRunner({ code: "print('some loaded code')" });
+  });
 
-beforeEach(() => {
-  window.crossOriginIsolated = true;
-  render(
-    <Provider store={store}>
-      <PythonRunner />
-    </Provider>,
-  );
-  updateRunner({ code: "print('some loaded code')" });
+  test("Renders with Pyodide runner initially", () => {
+    updateRunner({});
+    expect(
+      document.querySelector(".skulptrunner--active"),
+    ).not.toBeInTheDocument();
+    expect(
+      document.querySelector(".pyodiderunner--active"),
+    ).toBeInTheDocument();
+  });
+
+  test("Uses pyodide when no skulpt-only modules are imported", () => {
+    updateRunner({ code: "import math" });
+    expect(
+      document.querySelector(".skulptrunner--active"),
+    ).not.toBeInTheDocument();
+    expect(
+      document.querySelector(".pyodiderunner--active"),
+    ).toBeInTheDocument();
+  });
+
+  test("Uses skulpt when skulpt-only modules are imported", () => {
+    updateRunner({ code: "import p5" });
+    expect(
+      document.querySelector(".pyodiderunner--active"),
+    ).not.toBeInTheDocument();
+    expect(document.querySelector(".skulptrunner--active")).toBeInTheDocument();
+  });
+
+  test("Uses skulpt when senseHatAlwaysEnabled is true", () => {
+    updateRunner({ code: "import math", senseHatAlwaysEnabled: true });
+    expect(
+      document.querySelector(".pyodiderunner--active"),
+    ).not.toBeInTheDocument();
+    expect(document.querySelector(".skulptrunner--active")).toBeInTheDocument();
+  });
+
+  test("Uses skulpt if not cross origin isolated", () => {
+    window.crossOriginIsolated = false;
+    updateRunner({ code: "import math" });
+    expect(
+      document.querySelector(".pyodiderunner--active"),
+    ).not.toBeInTheDocument();
+    expect(document.querySelector(".skulptrunner--active")).toBeInTheDocument();
+  });
+
+  test("Switches runners if the import has a from clause", () => {
+    updateRunner({ code: "from p5 import *" });
+    expect(document.querySelector(".skulptrunner--active")).toBeInTheDocument();
+    expect(
+      document.querySelector(".pyodiderunner--active"),
+    ).not.toBeInTheDocument();
+  });
+
+  test("Switches runners if the import is indented", () => {
+    updateRunner({ code: "    import p5" });
+    expect(document.querySelector(".skulptrunner--active")).toBeInTheDocument();
+    expect(
+      document.querySelector(".pyodiderunner--active"),
+    ).not.toBeInTheDocument();
+  });
+
+  test("Uses skulpt if the py5 magic comment is used", () => {
+    updateRunner({ code: "# input.comment.py5" });
+    expect(document.querySelector(".skulptrunner--active")).toBeInTheDocument();
+    expect(
+      document.querySelector(".pyodiderunner--active"),
+    ).not.toBeInTheDocument();
+  });
+
+  test("Does not switch runners while the code is running", () => {
+    updateRunner({ code: "import p5", codeRunTriggered: true });
+    expect(
+      document.querySelector(".pyodiderunner--active"),
+    ).toBeInTheDocument();
+    expect(
+      document.querySelector(".skulptrunner--active"),
+    ).not.toBeInTheDocument();
+  });
+
+  test("Does not switch runners if the import is in a comment", () => {
+    updateRunner({ code: "# import p5" });
+    expect(
+      document.querySelector(".pyodiderunner--active"),
+    ).toBeInTheDocument();
+    expect(
+      document.querySelector(".skulptrunner--active"),
+    ).not.toBeInTheDocument();
+  });
+
+  test("Does not switch runners if the import is in a string", () => {
+    updateRunner({ code: 'print("import p5")' });
+    expect(
+      document.querySelector(".pyodiderunner--active"),
+    ).toBeInTheDocument();
+    expect(
+      document.querySelector(".skulptrunner--active"),
+    ).not.toBeInTheDocument();
+  });
+
+  test("Does not switch runners if the import is in a multiline string", () => {
+    updateRunner({ code: '"""\nimport p5\n"""' });
+    expect(
+      document.querySelector(".pyodiderunner--active"),
+    ).toBeInTheDocument();
+    expect(
+      document.querySelector(".skulptrunner--active"),
+    ).not.toBeInTheDocument();
+  });
+
+  test("Renders the text output in the skulpt runner", () => {
+    updateRunner({ code: "import p5" });
+    const skulptRunner = document.querySelector(".skulptrunner");
+    expect(
+      within(skulptRunner).queryByText("output.textOutput"),
+    ).toBeInTheDocument();
+  });
 });
 
-test("Renders with Pyodide runner initially", () => {
-  updateRunner({});
-  expect(
-    document.querySelector(".skulptrunner--active"),
-  ).not.toBeInTheDocument();
-  expect(document.querySelector(".pyodiderunner--active")).toBeInTheDocument();
-});
+describe("PythonRunner with output panels set to visual only", () => {
+  beforeEach(() => {
+    window.crossOriginIsolated = true;
+    render(
+      <Provider store={store}>
+        <PythonRunner outputPanels={["visual"]} />
+      </Provider>,
+    );
+    updateRunner({ code: "print('some loaded code')" });
+  });
 
-test("Uses pyodide when no skulpt-only modules are imported", () => {
-  updateRunner({ code: "import math" });
-  expect(
-    document.querySelector(".skulptrunner--active"),
-  ).not.toBeInTheDocument();
-  expect(document.querySelector(".pyodiderunner--active")).toBeInTheDocument();
-});
-
-test("Uses skulpt when skulpt-only modules are imported", () => {
-  updateRunner({ code: "import p5" });
-  expect(
-    document.querySelector(".pyodiderunner--active"),
-  ).not.toBeInTheDocument();
-  expect(document.querySelector(".skulptrunner--active")).toBeInTheDocument();
-});
-
-test("Uses skulpt when senseHatAlwaysEnabled is true", () => {
-  updateRunner({ code: "import math", senseHatAlwaysEnabled: true });
-  expect(
-    document.querySelector(".pyodiderunner--active"),
-  ).not.toBeInTheDocument();
-  expect(document.querySelector(".skulptrunner--active")).toBeInTheDocument();
-});
-
-test("Uses skulpt if not cross origin isolated", () => {
-  window.crossOriginIsolated = false;
-  updateRunner({ code: "import math" });
-  expect(
-    document.querySelector(".pyodiderunner--active"),
-  ).not.toBeInTheDocument();
-  expect(document.querySelector(".skulptrunner--active")).toBeInTheDocument();
-});
-
-test("Switches runners if the import has a from clause", () => {
-  updateRunner({ code: "from p5 import *" });
-  expect(document.querySelector(".skulptrunner--active")).toBeInTheDocument();
-  expect(
-    document.querySelector(".pyodiderunner--active"),
-  ).not.toBeInTheDocument();
-});
-
-test("Switches runners if the import is indented", () => {
-  updateRunner({ code: "    import p5" });
-  expect(document.querySelector(".skulptrunner--active")).toBeInTheDocument();
-  expect(
-    document.querySelector(".pyodiderunner--active"),
-  ).not.toBeInTheDocument();
-});
-
-test("Uses skulpt if the py5 magic comment is used", () => {
-  updateRunner({ code: "# input.comment.py5" });
-  expect(document.querySelector(".skulptrunner--active")).toBeInTheDocument();
-  expect(
-    document.querySelector(".pyodiderunner--active"),
-  ).not.toBeInTheDocument();
-});
-
-test("Does not switch runners while the code is running", () => {
-  updateRunner({ code: "import p5", codeRunTriggered: true });
-  expect(document.querySelector(".pyodiderunner--active")).toBeInTheDocument();
-  expect(
-    document.querySelector(".skulptrunner--active"),
-  ).not.toBeInTheDocument();
-});
-
-test("Does not switch runners if the import is in a comment", () => {
-  updateRunner({ code: "# import p5" });
-  expect(document.querySelector(".pyodiderunner--active")).toBeInTheDocument();
-  expect(
-    document.querySelector(".skulptrunner--active"),
-  ).not.toBeInTheDocument();
-});
-
-test("Does not switch runners if the import is in a string", () => {
-  updateRunner({ code: 'print("import p5")' });
-  expect(document.querySelector(".pyodiderunner--active")).toBeInTheDocument();
-  expect(
-    document.querySelector(".skulptrunner--active"),
-  ).not.toBeInTheDocument();
-});
-
-test("Does not switch runners if the import is in a multiline string", () => {
-  updateRunner({ code: '"""\nimport p5\n"""' });
-  expect(document.querySelector(".pyodiderunner--active")).toBeInTheDocument();
-  expect(
-    document.querySelector(".skulptrunner--active"),
-  ).not.toBeInTheDocument();
+  test("Does not render text output in the skulpt runner", () => {
+    updateRunner({ code: "import p5" });
+    const skulptRunner = document.querySelector(".skulptrunner");
+    expect(
+      within(skulptRunner).queryByText("output.textOutput"),
+    ).not.toBeInTheDocument();
+  });
 });
