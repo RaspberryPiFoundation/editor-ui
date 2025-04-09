6afda2e9879331b9f542c2703e1ab9b559f121ad
Lois Wells <lois.wells@raspberrypi.org>
2025-04-09 09:15:16 +0100

    using factored out getImports util in pythonrunner

--- src/components/Editor/Runners/PythonRunner/PythonRunner.jsx
+++ src/components/Editor/Runners/PythonRunner/PythonRunner.jsx
@@ -6,6 +6,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { useTranslation } from "react-i18next";
 
 import { loadingRunner } from "../../../../redux/EditorSlice";
+import { getImports } from "../../../../utils/getImports";
 
 const SKULPT_ONLY_MODULES = [
   "p5",
@@ -45,28 +46,6 @@ const PythonRunner = ({ outputPanels = ["text", "visual"] }) => {
       setSkulptFallback(true);
       return;
     }
-    const getImports = (code) => {
-      const codeWithoutMultilineStrings = code.replace(
-        /'''[\s\S]*?'''|"""[\s\S]*?"""/gm,
-        "",
-      );
-      const importRegex =
-        /(?<=^\s*)(from\s+([a-zA-Z0-9_.]+)(\s+import\s+([a-zA-Z0-9_.]+))?)|(?<=^\s*)(import\s+([a-zA-Z0-9_.]+))/gm;
-      const matches = codeWithoutMultilineStrings.match(importRegex);
-      const imports = matches
-        ? matches.map(
-            (match) =>
-              match
-                .split(/from|import/)
-                .filter(Boolean)
-                .map((s) => s.trim())[0],
-          )
-        : [];
-      if (code.includes(`# ${t("input.comment.py5")}`)) {
-        imports.push("py5_imported");
-      }
-      return imports;
-    };
 
     for (const component of project.components || []) {
       if (component.extension === "py" && !codeRunTriggered) {
