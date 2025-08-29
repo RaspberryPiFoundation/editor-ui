a898fc0a381f6a936e29ac9f3ed9e6180ecaac8d
Jamie Benstead <jamie.benstead@gmail.com>
2025-08-29 12:51:21 +0100

    Update test

--- cypress/e2e/spec-wc-skulpt.cy.js
+++ cypress/e2e/spec-wc-skulpt.cy.js
@@ -88,7 +88,11 @@ describe("Running the code with skulpt", () => {
 
   it("Py5 magic comment imports py5", () => {
     runCode("# Py5: imported mode");
-    cy.get("editor-wc").shadow().find(".p5Canvas").should("be.visible").debug();
+    cy.get("editor-wc")
+      .shadow()
+      .find("input.comment.py5")
+      .should("be.visible")
+      .debug();
   });
 
   it("Py5 imported mode runs sketch without explicit run call", () => {
