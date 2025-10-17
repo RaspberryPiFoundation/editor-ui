1b022b98765522ab7cf958f43cd6c365fc96b8c9
Max Elkins <max.elkins@raspberrypi.org>
2025-10-18 00:19:24 +0100

    refactor: RunnerControls to use DS Button

--- src/components/RunButton/RunnerControls.jsx
+++ src/components/RunButton/RunnerControls.jsx
@@ -2,14 +2,13 @@ import React from "react";
 import RunButton from "./RunButton";
 import StopButton from "./StopButton";
 import { useSelector } from "react-redux";
-import RunIcon from "../../assets/icons/run.svg";
-import StopIcon from "../../assets/icons/stop.svg";
+
 import { useTranslation } from "react-i18next";
 import "../../assets/stylesheets/RunnerControls.scss";
 
 const RunnerControls = ({ embedded = false, skinny = false }) => {
   const codeRunTriggered = useSelector(
-    (state) => state.editor.codeRunTriggered,
+    (state) => state.editor.codeRunTriggered
   );
   const drawTriggered = useSelector((state) => state.editor.drawTriggered);
   const { t } = useTranslation();
@@ -19,20 +18,20 @@ const RunnerControls = ({ embedded = false, skinny = false }) => {
       {codeRunTriggered || drawTriggered ? (
         <StopButton
           embedded={embedded}
-          buttonText={t("runButton.stop")}
-          ButtonIcon={StopIcon}
-          buttonIconPosition="right"
-          buttonOuter={skinny}
-          className={`btn--stop btn--primary${skinny ? " btn--small" : ""}`}
+          text={t("runButton.stop")}
+          icon="stop"
+          iconPosition="right"
+          // buttonOuter={skinny}
+          // className={`btn--stop btn--primary${skinny ? " btn--small" : ""}`}
         />
       ) : (
         <RunButton
           embedded={embedded}
-          buttonText={t("runButton.run")}
-          ButtonIcon={RunIcon}
-          buttonIconPosition="right"
-          buttonOuter={skinny}
-          className={skinny ? "btn--small" : ""}
+          text={t("runButton.run")}
+          icon="play_arrow"
+          iconPosition="right"
+          // buttonOuter={skinny}
+          // className={skinny ? "btn--small" : ""}
         />
       )}
     </div>
