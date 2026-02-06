76aa29c49ccf2e59f8ca00afe02ac3347e6dbc28
Chris Zetter <253059100+zetter-rpf@users.noreply.github.com>
2026-02-06 16:45:16 +0000

    Serve scratch static assets
    This stops some images 404ing such as the icons used in some of the blocks. This approach was copied from the
    experience CS repo.
    
    I've set assetHost so the assets are loaded from the deployed web component site rather than relative to the project.

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,16 @@ if (!publicUrl.endsWith("/")) {
   publicUrl += "/";
 }
 
+const scratchStaticAssetDir = path.resolve(
+  __dirname,
+  "node_modules/@scratch/scratch-gui/dist/static",
+)
+
+const scratchChunkDir = path.resolve(
+  __dirname,
+  "node_modules/@scratch/scratch-gui/dist/chunks",
+)
+
 module.exports = {
   entry: {
     "web-component": path.resolve(__dirname, "./src/web-component.js"),
@@ -111,6 +121,14 @@ module.exports = {
         directory: path.join(__dirname, "src", "projects"),
         publicPath: `${publicUrl}projects`,
       },
+      {
+        directory: scratchStaticAssetDir,
+        publicPath: `${publicUrl}scratch-gui/static`,
+      },
+      {
+        directory: scratchChunkDir,
+        publicPath: `${publicUrl}scratch-gui/chunks`,
+      }
     ],
     headers: {
       "Access-Control-Allow-Origin": "*",
@@ -152,6 +170,8 @@ module.exports = {
     }),
     new CopyWebpackPlugin({
       patterns: [
+        { from: scratchStaticAssetDir, to: "scratch-gui/static" },
+        { from: scratchChunkDir, to: "scratch-gui/chunks" },
         { from: "public", to: "" },
         { from: "src/projects", to: "projects" },
       ],
