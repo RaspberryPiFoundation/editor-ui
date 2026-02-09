6c3c1b56ac2e4f27b9eac46abbebdf97250a878b
Chris Zetter <253059100+zetter-rpf@users.noreply.github.com>
2026-02-09 16:56:22 +0000

    End to end scratch test
    This test makes sure that scratch is rendering by checking the 'Go' button (green flag).
    
    We might want to update it to test more interesting behaviour later.

---
+++ cypress/e2e/spec-scratch.cy.js
@@ -0,0 +1,30 @@
+const origin = "http://localhost:3011/web-component.html";
+
+beforeEach(() => {
+  cy.intercept("*", (req) => {
+    req.headers["Origin"] = origin;
+    req.continue();
+  });
+  cy.viewport(1400, 800);
+});
+
+const getIframeBody = () => {
+  return cy
+    .get("editor-wc")
+    .shadow()
+    .findByTitle("Scratch")
+    .its("0.contentDocument.body")
+    .should("not.be.empty")
+    .then(cy.wrap);
+};
+
+describe("Scratch", () => {
+  beforeEach(() => {
+    cy.visit(origin);
+    cy.findByText("blank-scratch").click();
+  });
+
+  it("loads Scratch in an iframe", () => {
+    getIframeBody().find("button [title='Go']").should("be.visible");
+  });
+});
