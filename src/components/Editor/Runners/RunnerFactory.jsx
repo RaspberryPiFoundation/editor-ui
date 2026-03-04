7b0bc97de58ea0d9de6c789620f6d332bb8a3b3a
Chris Zetter <253059100+zetter-rpf@users.noreply.github.com>
2026-03-04 15:03:59 +0000

    Ruby runner

--- src/components/Editor/Runners/RunnerFactory.jsx
+++ src/components/Editor/Runners/RunnerFactory.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import PythonRunner from "./PythonRunner/PythonRunner";
+import RubyRunner from "./RubyRunner/RubyRunner";
 import HtmlRunner from "./HtmlRunner/HtmlRunner";
 
 const RunnerFactory = ({ projectType, outputPanels = ["text", "visual"] }) => {
   const Runner = () => {
     if (projectType === "html") {
       return HtmlRunner;
+    } else if (projectType === "ruby") {
+      return RubyRunner;
     } else {
       return PythonRunner;
     }
