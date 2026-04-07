021daa8a33e5ec1951955e7ea38da94fd6b1c4f6
Matthew Trew <matthew.trew@raspberrypi.org>
2026-04-07 10:59:58 +0100

    Skip HTML editor tests
    These are not currently working on CI. We may move these to the E2E test
    repo.

--- cypress/e2e/spec-html.cy.js
+++ cypress/e2e/spec-html.cy.js
@@ -39,151 +39,156 @@ const clickRunButton = () => {
   return cy.get("editor-wc").shadow().find(".btn--run").click();
 };
 
-beforeEach(() => {
-  // intercept request to editor api
-  cy.intercept(
-    "GET",
-    `${Cypress.env(
-      "REACT_APP_API_ENDPOINT",
-    )}/api/projects/blank-html-starter?locale=en`,
-    defaultHtmlProject,
-  );
-});
+describe.skip("HTML editor", () => {
+  beforeEach(() => {
+    // intercept request to editor api
+    cy.intercept(
+      "GET",
+      `${Cypress.env(
+        "REACT_APP_API_ENDPOINT",
+      )}/api/projects/blank-html-starter?locale=en`,
+      defaultHtmlProject,
+    );
+  });
 
-it("blocks access to parent localStorage", () => {
-  // Arrange
-  localStorage.clear();
-  localStorage.setItem("parentKey", "secretValue");
+  it("blocks access to parent localStorage", () => {
+    // Arrange
+    localStorage.clear();
+    localStorage.setItem("parentKey", "secretValue");
 
-  cy.visit(baseUrl);
+    cy.visit(baseUrl);
 
-  // Act
-  const input = getEditorInput();
+    // Act
+    const input = getEditorInput();
 
-  input.invoke(
-    "text",
-    `<p>parentKey: <span id="s"></span></p>
+    input.invoke(
+      "text",
+      `<p>parentKey: <span id="s"></span></p>
 <script>
   const authKey = localStorage.getItem("parentKey")
   document.getElementById("s").innerHTML = \`\${authKey}\`
 </script>`,
-  );
-  clickRunButton();
+    );
+    clickRunButton();
 
-  // Assert
-  getIframeBody().find("p").should("include.text", "parentKey: null");
-});
+    // Assert
+    getIframeBody().find("p").should("include.text", "parentKey: null");
+  });
 
-it("allows access to localStorage", () => {
-  // Arrange
-  localStorage.clear();
-  cy.visit(baseUrl);
+  it("allows access to localStorage", () => {
+    // Arrange
+    localStorage.clear();
+    cy.visit(baseUrl);
 
-  // Act
-  const input = getEditorInput();
+    // Act
+    const input = getEditorInput();
 
-  input.invoke(
-    "text",
-    `<p>foo: <span id="foo"></span></p>
+    input.invoke(
+      "text",
+      `<p>foo: <span id="foo"></span></p>
 <script>
   localStorage.setItem("foo", "bar")
   const foo = localStorage.getItem("foo")
   document.getElementById("foo").innerHTML = \`\${foo}\`
 </script>`,
-  );
+    );
 
-  clickRunButton();
+    clickRunButton();
 
-  // Assert
-  getIframeBody().find("p").should("include.text", "foo: bar");
-});
+    // Assert
+    getIframeBody().find("p").should("include.text", "foo: bar");
+  });
 
-it("renders the html runner", () => {
-  cy.visit(baseUrl);
-  cy.get("editor-wc").shadow().find(".btn--run").click();
-  cy.get("editor-wc")
-    .shadow()
-    .find(".htmlrunner-container")
-    .should("be.visible");
-});
+  it("renders the html runner", () => {
+    cy.visit(baseUrl);
+    cy.get("editor-wc").shadow().find(".btn--run").click();
+    cy.get("editor-wc")
+      .shadow()
+      .find(".htmlrunner-container")
+      .should("be.visible");
+  });
 
-it("can make a new file", () => {
-  cy.visit(baseUrl);
-  makeNewFile("amazing.html");
-  cy.get("editor-wc")
-    .shadow()
-    .find(".files-list-item")
-    .should("include.text", "amazing.html");
-});
+  it("can make a new file", () => {
+    cy.visit(baseUrl);
+    makeNewFile("amazing.html");
+    cy.get("editor-wc")
+      .shadow()
+      .find(".files-list-item")
+      .should("include.text", "amazing.html");
+  });
 
-it("updates the preview after a change when you click run", () => {
-  localStorage.clear();
-  cy.visit(baseUrl);
-  cy.get("editor-wc").shadow().find(".btn--run").click();
-  getIframeBody().should("not.include.text", "hello world");
-  cy.get("editor-wc")
-    .shadow()
-    .find("div[class=cm-content]")
-    .invoke("text", "<p>hello world</p>");
-  cy.get("editor-wc").shadow().find(".btn--run").click();
-  getIframeBody().find("p").should("include.text", "hello world");
-});
+  it("updates the preview after a change when you click run", () => {
+    localStorage.clear();
+    cy.visit(baseUrl);
+    cy.get("editor-wc").shadow().find(".btn--run").click();
+    getIframeBody().should("not.include.text", "hello world");
+    cy.get("editor-wc")
+      .shadow()
+      .find("div[class=cm-content]")
+      .invoke("text", "<p>hello world</p>");
+    cy.get("editor-wc").shadow().find(".btn--run").click();
+    getIframeBody().find("p").should("include.text", "hello world");
+  });
 
-it("blocks non-permitted external links", () => {
-  // Arrange
-  localStorage.clear();
-  cy.visit(baseUrl);
+  it("blocks non-permitted external links", () => {
+    // Arrange
+    localStorage.clear();
+    cy.visit(baseUrl);
 
-  // Act
-  getEditorInput().invoke(
-    "text",
-    '<a href="https://raspberrypi.org/en/">some external link</a>',
-  );
-  clickRunButton();
+    // Act
+    getEditorInput().invoke(
+      "text",
+      '<a href="https://raspberrypi.org/en/">some external link</a>',
+    );
+    clickRunButton();
 
-  getIframeBody().find("a").click();
+    getIframeBody().find("a").click();
 
-  // Assert
-  cy.get("editor-wc")
-    .shadow()
-    .find("div[class=modal-content__header]")
-    .find("h2")
-    .should("include.text", "An error has occurred");
-});
+    // Assert
+    cy.get("editor-wc")
+      .shadow()
+      .find("div[class=modal-content__header]")
+      .find("h2")
+      .should("include.text", "An error has occurred");
+  });
 
-it("allows permitted external links", () => {
-  localStorage.clear();
-  cy.visit(baseUrl);
-  cy.get("editor-wc")
-    .shadow()
-    .find("div[class=cm-content]")
-    .invoke("text", '<a href="https://rpf.io/seefood">some external link</a>');
-  cy.get("editor-wc").shadow().find(".btn--run").click();
-  const externalLink = getIframeBody().find("a");
-  externalLink.click();
-  cy.url().should("be.equals", baseUrl);
-  cy.get("editor-wc")
-    .shadow()
-    .find("div[class=modal-content__header]")
-    .should("not.exist");
-});
+  it("allows permitted external links", () => {
+    localStorage.clear();
+    cy.visit(baseUrl);
+    cy.get("editor-wc")
+      .shadow()
+      .find("div[class=cm-content]")
+      .invoke(
+        "text",
+        '<a href="https://rpf.io/seefood">some external link</a>',
+      );
+    cy.get("editor-wc").shadow().find(".btn--run").click();
+    const externalLink = getIframeBody().find("a");
+    externalLink.click();
+    cy.url().should("be.equals", baseUrl);
+    cy.get("editor-wc")
+      .shadow()
+      .find("div[class=modal-content__header]")
+      .should("not.exist");
+  });
 
-it("allows internal links", () => {
-  localStorage.clear();
-  cy.visit(baseUrl);
-  cy.get("editor-wc")
-    .shadow()
-    .find("div[class=cm-content]")
-    .invoke("text", "<p>hello world</p>");
-  cy.get("editor-wc").shadow().find(".btn--run").click();
-  makeNewFile();
-  cy.get("editor-wc")
-    .shadow()
-    .find("div[class=cm-content]")
-    .invoke("text", '<a href="index.html">some internal link</a>');
-  cy.get("editor-wc").shadow().find(".btn--run").click();
+  it("allows internal links", () => {
+    localStorage.clear();
+    cy.visit(baseUrl);
+    cy.get("editor-wc")
+      .shadow()
+      .find("div[class=cm-content]")
+      .invoke("text", "<p>hello world</p>");
+    cy.get("editor-wc").shadow().find(".btn--run").click();
+    makeNewFile();
+    cy.get("editor-wc")
+      .shadow()
+      .find("div[class=cm-content]")
+      .invoke("text", '<a href="index.html">some internal link</a>');
+    cy.get("editor-wc").shadow().find(".btn--run").click();
 
-  const internalLink = getIframeBody().find("a");
-  internalLink.click();
-  getIframeBody().should("contain.text", "hello world");
+    const internalLink = getIframeBody().find("a");
+    internalLink.click();
+    getIframeBody().should("contain.text", "hello world");
+  });
 });
