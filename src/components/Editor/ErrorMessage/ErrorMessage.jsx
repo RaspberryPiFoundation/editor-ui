497147492855deb9e91e0dc9d2fae664034f3f67
Max Elkins <max.elkins@raspberrypi.org>
2026-05-29 17:53:39 +0100

    feat: add initial FriendlyErrorMessage UI component into ErrorMessage

--- src/components/Editor/ErrorMessage/ErrorMessage.jsx
+++ src/components/Editor/ErrorMessage/ErrorMessage.jsx
@@ -3,12 +3,17 @@ import "../../../assets/stylesheets/ErrorMessage.scss";
 import { useSelector } from "react-redux";
 import DOMPurify from "dompurify";
 import { SettingsContext } from "../../../utils/settings";
+import CancelFillIcon from "../../../assets/icons/cancel_FILL.svg";
+import FriendlyErrorMessage from "../FriendlyErrorMessage/FriendlyErrorMessage";
 
 const ErrorMessage = () => {
   const message = useRef();
   const error = useSelector((state) => state.editor.error);
   const friendlyError = useSelector((state) => state.editor.friendlyError);
   const settings = useContext(SettingsContext);
+
+  // To be removed when ready
+  const fem = false;
 
   useEffect(() => {
     if (message.current) {
@@ -22,6 +27,7 @@ const ErrorMessage = () => {
 
   return error ? (
     <div className={`error-message error-message--${settings.fontSize}`}>
+      <CancelFillIcon />
       <pre ref={message} className="error-message__content" />
       {friendlyErrorHtml && (
         <div
