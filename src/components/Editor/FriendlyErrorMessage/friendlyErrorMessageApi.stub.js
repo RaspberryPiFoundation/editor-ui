8bd51f84c6c272d0e378a5931191232604618c49
cocomarine <hj.kang@raspberrypi.org>
2026-06-03 14:29:59 +0100

    remove fem stub, add friendly test, revert error message test

--- src/components/Editor/FriendlyErrorMessage/friendlyErrorMessageApi.stub.js
+++
@@ -1,7 +0,0 @@
-// To be removed once Python FEM is added
-export const getFriendlyErrorMessage = async () => {
-  return Promise.resolve({
-    title: "This variable doesn't exist yet",
-    summary: `Your code uses the variable "total", but it hasn't been created yet. Check line 3 in main.py. If you meant to print the text total, put it in double quotes.`,
-  });
-};
