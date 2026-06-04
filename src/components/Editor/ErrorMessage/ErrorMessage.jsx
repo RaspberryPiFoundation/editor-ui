da8c41160147bdfe7a5d2aa7dcc0087e34078ce0
cocomarine <hj.kang@raspberrypi.org>
2026-06-04 09:16:08 +0100

    resolve conflicts

--- src/components/Editor/ErrorMessage/ErrorMessage.jsx
+++ src/components/Editor/ErrorMessage/ErrorMessage.jsx
@@ -8,15 +8,7 @@ import FriendlyErrorMessage from "../FriendlyErrorMessage/FriendlyErrorMessage";
 
 const ErrorMessage = () => {
   const error = useSelector((state) => state.editor.error);
-  const friendlyError = useSelector((state) => state.editor.friendlyError);
   const settings = useContext(SettingsContext);
-
-<<<<<<< 1448-and-ui-branches-combined
-  useEffect(() => {
-    if (message.current) {
-      message.current.innerHTML = error;
-    }
-  }, [error]);
 
   return error ? (
     <div className="error-message">
@@ -24,29 +16,12 @@ const ErrorMessage = () => {
         className={`error-message__python error-message__python--${settings.fontSize}`}
       >
         <CancelFillIcon />
-        <pre ref={message} className="error-message__error" />
+        <pre
+          className="error-message__error"
+          dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(error) }}
+        />
       </div>
       <FriendlyErrorMessage />
-=======
-  const errorHtml = DOMPurify.sanitize(error);
-
-  const friendlyErrorHtml = friendlyError?.html
-    ? DOMPurify.sanitize(friendlyError.html)
-    : null;
-
-  return error ? (
-    <div className={`error-message error-message--${settings.fontSize}`}>
-      <pre
-        className="error-message__content"
-        dangerouslySetInnerHTML={{ __html: errorHtml }}
-      />
-      {friendlyErrorHtml && (
-        <div
-          className="error-message__friendly"
-          dangerouslySetInnerHTML={{ __html: friendlyErrorHtml }}
-        />
-      )}
->>>>>>> main
     </div>
   ) : null;
 };
