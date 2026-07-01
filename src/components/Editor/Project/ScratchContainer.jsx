3c2e7e697b37901031082a8a01d89dea78108c69
Chris Zetter <253059100+zetter-rpf@users.noreply.github.com>
2026-07-01 15:14:50 +0100

    fix Scratch Container

--- src/components/Editor/Project/ScratchContainer.jsx
+++ src/components/Editor/Project/ScratchContainer.jsx
@@ -126,7 +126,7 @@ export default function ScratchContainer() {
   queryParams.set("scratchMetadata", "1");
   queryParams.set("parent_origin", window.location.origin);
 
-  const iframeSrcUrl = `${import.meta.env.REACT_APP_SCRATCH_FRAME_URL}/scratch.html?${queryParams.toString()}`;
+  const iframeSrcUrl = `${process.env.REACT_APP_SCRATCH_FRAME_URL}/scratch.html?${queryParams.toString()}`;
 
   return (
     <div className="scratch-container" data-testid="scratch-container">
